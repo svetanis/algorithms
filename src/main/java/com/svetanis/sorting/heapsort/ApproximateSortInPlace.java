7757a9213fc052779f098a43981438c3e1df531d
sveta <svet125@gmail.com>
2019-04-06 20:21:19 -0700

    [sveta] K sorted array in place sorting

--- src/main/java/com/svetanis/sorting/heapsort/ApproximateSortInPlace.java
+++ src/main/java/com/svetanis/sorting/heapsort/ApproximateSortInPlace.java
@@ -1,33 +1,32 @@
 package com.svetanis.sorting.heapsort;
 
 import static com.google.common.collect.Lists.newArrayList;
-import static com.svetanis.java.base.collect.Lists.newList;
 import static com.svetanis.java.base.utils.Print.print;
 
 import java.util.List;
 import java.util.PriorityQueue;
 import java.util.Queue;
 
-import com.google.common.collect.ImmutableList;
+public final class ApproximateSortInPlace {
 
-public final class ApproximateSort {
-
-  public static ImmutableList<Integer> sort(int[] a, int k) {
+  public static void sort(int[] a, int k) {
     // Time Complexity: O(n log k)
-    // Auxiliary Space: O(n)
+    // Auxiliary Space: O(k)
 
+    int n = a.length;
     Queue<Integer> queue = init(a, k);
     List<Integer> list = newArrayList();
-    for (int j = k; j < a.length; j++) {
-      queue.offer(a[j]);
-      list.add(queue.poll());
+    for (int i = k, j = 0; j < n; i++, j++) {
+      a[j] = queue.poll();
+      if (i < n) {
+        queue.offer(a[i]);
+      }
     }
 
     // extract the remaining elements in queue
     while (!queue.isEmpty()) {
       list.add(queue.poll());
     }
-    return newList(list);
   }
 
   private static Queue<Integer> init(int[] a, int k) {
@@ -41,6 +40,8 @@ public final class ApproximateSort {
 
   public static void main(String[] args) {
     int[] a = { 2, 6, 3, 12, 56, 8 };
-    print(sort(a, 3));
+    sort(a, 3);
+    print(a);
   }
+
 }
