387117dd759fec58479173eaee8fbf5cb50e9df3
sveta <svet125@gmail.com>
2019-04-06 20:32:53 -0700

    [sveta] renaming

---
+++ src/main/java/com/svetanis/algorithms/sorting/heapsort/ApproximateSort.java
@@ -0,0 +1,5 @@
+Unstaged changes after reset:
+M	src/main/java/com/svetanis/algorithms/sorting/heapsort/ApproximateSort.java
+M	src/main/java/com/svetanis/algorithms/sorting/heapsort/ApproximateSortInPlace.java
+M	src/main/java/com/svetanis/algorithms/sorting/heapsort/HeapSort.java
+D	src/main/java/com/svetanis/sorting/heapsort/ApproximateSort.java
