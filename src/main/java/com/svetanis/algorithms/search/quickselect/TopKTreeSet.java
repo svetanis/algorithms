199a235a8eda57052a52f2ba377edd77d1e4c0f8
sveta <svet125@gmail.com>
2019-04-13 22:44:30 -0700

    [sveta] top k tree set implementation

---
+++ src/main/java/com/svetanis/algorithms/search/quickselect/TopKTreeSet.java
@@ -0,0 +1,31 @@
+package com.svetanis.algorithms.search.quickselect;
+
+import static com.google.common.collect.ImmutableList.copyOf;
+import static com.google.common.collect.Sets.newTreeSet;
+
+import java.util.TreeSet;
+
+import com.google.common.collect.ImmutableList;
+
+public final class TopKTreeSet {
+
+  public static ImmutableList<Integer> topK(int a[], int k) {
+    // Time Complexity: O(n log k)
+
+    TreeSet<Integer> ts = newTreeSet();
+    for (int item : a) {
+      if (ts.size() < k || item > ts.first()) {
+        if (ts.size() == k) {
+          ts.pollFirst(); // remove smallest 
+        }
+        ts.add(item);
+      }
+    }
+    return copyOf(ts);
+  }
+
+  public static void main(String[] args) {
+    int[] a = { 10, 2, 4, 6, 8, 1, 3, 5, 7, 10, 128 };
+    System.out.println(topK(a, 4));
+  }
+}
