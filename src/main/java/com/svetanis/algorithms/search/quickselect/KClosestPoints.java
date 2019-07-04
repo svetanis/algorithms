dd79c167d3c293d5bce636dd7c6de9e54c8c6001
sveta <svet125@gmail.com>
2019-07-04 15:23:35 -0700

    [sveta] k closest points

--- src/main/java/com/svetanis/algorithms/search/quickselect/KClosestPoints.java
+++ src/main/java/com/svetanis/algorithms/search/quickselect/KClosestPoints.java
@@ -1,33 +1,33 @@
 package com.svetanis.algorithms.search.quickselect;
 
+import static com.google.common.collect.Lists.newArrayList;
+import static com.svetanis.java.base.collect.Lists.newList;
 import static com.svetanis.java.base.utils.Print.print;
 import static com.svetanis.java.base.utils.Random.randomIndex;
 import static com.svetanis.java.base.utils.Swap.swap;
 import static java.lang.Math.pow;
-import static java.util.Arrays.asList;
-import static java.util.Arrays.copyOf;
 
 import java.awt.Point;
-import java.util.Comparator;
 import java.util.List;
+
+import com.google.common.collect.ImmutableList;
 
 public final class KClosestPoints {
 
-  private static final Comparator<Point> COMPARATOR = new PointComparator();
-
-  public static List<Point> select(Point[] a, int k) {
+  public static ImmutableList<Point> select(List<Point> points, int k) {
     // Time complexity: O(n)
     // Worst case: O(n^2)
 
-    int n = a.length;
-    if (k <= 0) {
-      return asList(new Point());
+    Distance[] a = distances(points);
+    int pivot = select(a, 0, a.length - 1, k);
+    List<Point> list = newArrayList();
+    for (int i = 0; i <= pivot; i++) {
+      list.add(points.get(a[i].index));
     }
-    int pivot = select(a, 0, n - 1, k);
-    return asList(copyOf(a, pivot + 1));
+    return newList(list);
   }
 
-  private static int select(Point[] a, int left, int right, int k) {
+  private static int select(Distance[] a, int left, int right, int k) {
     int index = randomIndex(left, right);
     int pivot = partition(a, left, right, index);
     int dist = pivot - left + 1;
@@ -41,12 +41,12 @@ public final class KClosestPoints {
   }
 
   // smaller elements to the left
-  public static int partition(Point[] a, int left, int right, int index) {
+  public static int partition(Distance[] a, int left, int right, int index) {
     int i = left;
     swap(a, right, index);
-    Point pivot = a[right];
+    Distance pivot = a[right];
     for (int j = left; j < right; ++j) {
-      if (COMPARATOR.compare(a[j], pivot) < 1) {
+      if (a[j].compareTo(pivot) < 1) {
         swap(a, i, j);
         i++;
       }
@@ -57,39 +57,50 @@ public final class KClosestPoints {
   }
 
   public static void main(String[] args) {
-    Point[] points = points();
+    List<Point> points = points();
     print(select(points, 2));
   }
 
-  private static class PointComparator implements Comparator<Point> {
+  private static Distance[] distances(List<Point> points) {
+    Distance[] a = new Distance[points.size()];
+    for (int i = 0; i < points.size(); i++) {
+      a[i] = new Distance(distToOrigin(points.get(i)), i);
+    }
+    return a;
+  }
 
-    private final Point origin = new Point();
+  private static ImmutableList<Point> points() {
+    List<Point> list = newArrayList();
+    list.add(new Point(1, 0));
+    list.add(new Point(2, 1));
+    list.add(new Point(0, 1));
+    return newList(list);
+  }
 
-    private double dist(Point p1, Point p2) {
-      double dx = p1.getX() - p2.getX();
-      double dy = p1.getY() - p2.getY();
-      return pow(dx, 2) + pow(dy, 2);
+  private static int distToOrigin(Point p) {
+    return dist(p, new Point(0, 0));
+  }
+
+  private static int dist(Point p1, Point p2) {
+    double dx = p1.getX() - p2.getX();
+    double dy = p1.getY() - p2.getY();
+    return new Double(pow(dx, 2) + pow(dy, 2)).intValue();
+  }
+
+  private static class Distance implements Comparable<Distance> {
+
+    private int dist;
+    private int index;
+
+    public Distance(int dist, int index) {
+      this.dist = dist;
+      this.index = index;
     }
 
     @Override
-    public int compare(Point p1, Point p2) {
-      double d1 = dist(p1, origin);
-      double d2 = dist(p2, origin);
-      if (d1 < d2) {
-        return -1;
-      }
-      if (d1 == d2) {
-        return 0;
-      }
-      return 1;
+    public int compareTo(Distance other) {
+      return this.dist - other.dist;
     }
   }
 
-  private static Point[] points() {
-    Point[] points = new Point[3];
-    points[0] = new Point(3, 3);
-    points[1] = new Point(5, -1);
-    points[2] = new Point(-2, 4);
-    return points;
-  }
 }
