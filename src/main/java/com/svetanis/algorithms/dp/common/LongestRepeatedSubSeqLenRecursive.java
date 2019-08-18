ed6e93df4a1af33effb538a1ce21ea514fc975cf
sveta <svet125@gmail.com>
2019-08-17 18:44:25 -0700

    [sveta] longest repeated subseq

--- src/main/java/com/svetanis/algorithms/dp/common/LongestRepeatedSubSeqLenRecursive.java
+++ src/main/java/com/svetanis/algorithms/dp/common/LongestRepeatedSubSeqLenRecursive.java
@@ -2,6 +2,12 @@ package com.svetanis.algorithms.dp.common;
 
 import static java.lang.Math.max;
 
+// Given a sequence, find the length of its longest repeating subsequence (LRS). 
+// A repeating subsequence will be the one that appears at least twice 
+// in the original sequence and is not overlapping 
+// (i.e. none of the corresponding characters 
+// in the repeating subsequences have the same index).
+
 public final class LongestRepeatedSubSeqLenRecursive {
 
   public static int lcs(String str) {
