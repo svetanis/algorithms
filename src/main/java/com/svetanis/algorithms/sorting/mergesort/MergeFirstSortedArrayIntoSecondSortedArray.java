1b898272fc7bcd89fc4e112883edaca4345d4e3c
sveta <svet125@gmail.com>
2019-08-21 21:25:24 -0700

    [sveta] merge first sorted array into second sorted array

--- src/main/java/com/svetanis/algorithms/sorting/mergesort/MergeFirstSortedArrayIntoSecondSortedArray.java
+++ src/main/java/com/svetanis/algorithms/sorting/mergesort/MergeFirstSortedArrayIntoSecondSortedArray.java
@@ -1,38 +1,56 @@
 package com.svetanis.algorithms.sorting.mergesort;
 
+import static com.google.common.collect.Lists.newArrayList;
+import static com.svetanis.java.base.collect.Lists.newList;
 import static com.svetanis.java.base.utils.Print.print;
+
+import java.util.List;
+
+import com.google.common.collect.ImmutableList;
 
 public final class MergeFirstSortedArrayIntoSecondSortedArray {
 
-  public static int[] merge(int[] smaller, int[] larger) {
+  public static ImmutableList<Integer> merge(List<Integer> larger, List<Integer> smaller) {
     // Time complexity: O(n)
     // Auxiliary Space: O(1)
     // Space Complexity: O(n) due to input size
 
-    int n = larger.length;
-    int m = smaller.length;
+    int n = larger.size();
+    int m = smaller.size();
 
-    int i = m - 1; // current index of input part of largerArray
+    int i = n - 1; // current index of input part of largerArray
     int j = m - 1; // current index of smallerArray
-    int k = n - 1; // current index of output part of largerArray
+    int k = n + m - 1; // current index of output part of largerArray
 
-    while (k >= 0) {
-      if (i < 0) {
-        larger[k--] = smaller[j--];
-      } else if (j < 0) {
-        larger[k--] = larger[i--];
-      } else if (larger[i] >= smaller[j]) {
-        larger[k--] = larger[i--];
+    ensureSize(larger, k + 1);
+    n = larger.size();
+
+    while (j >= 0) {
+      if (i >= 0 && larger.get(i) > smaller.get(j)) {
+        larger.set(k, larger.get(i));
+        i--;
       } else {
-        larger[k--] = smaller[j--];
+        larger.set(k, smaller.get(j));
+        j--;
       }
+      k--;
     }
-    return larger;
+    return newList(larger);
+  }
+
+  public static void ensureSize(List<Integer> list, int size) {
+    while (list.size() < size) {
+      list.add(null);
+    }
   }
 
   public static void main(String[] args) {
-    int[] smaller = { 1, 3, 5 };
-    int[] larger = { 2, 4, 6, 0, 0, 0 };
-    print(merge(smaller, larger));
+    List<Integer> smaller = newArrayList(1, 3, 5);
+    List<Integer> larger = newArrayList(2, 4, 6);
+    print(merge(larger, smaller));
+
+    List<Integer> small = newArrayList(2);
+    List<Integer> large = newArrayList(-4, -3, 0);
+    print(merge(large, small));
   }
 }
