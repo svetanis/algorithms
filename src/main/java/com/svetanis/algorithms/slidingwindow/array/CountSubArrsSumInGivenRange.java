617b448bded01d8df620311122e76255ff73d39f
svetanis <svet125@gmail.com>
2024-04-13 16:02:36 -0700

    count subarray with sum in given range

--- src/main/java/com/svetanis/algorithms/slidingwindow/array/CountSubArrsSumInGivenRange.java
+++ src/main/java/com/svetanis/algorithms/slidingwindow/array/CountSubArrsSumInGivenRange.java
@@ -1,24 +1,44 @@
 package com.svetanis.algorithms.slidingwindow.array;
 
-import static com.svetanis.algorithms.slidingwindow.array.CountSubArrsProductLessThanK.count;
+// given array of positive integers and range (L,R)
+// find number of subarrays with sum in the given range
 
 public final class CountSubArrsSumInGivenRange {
 
-  public static int countInRange(int[] a, int start, int end) {
-    // Time Complexity: O(n)
-    // Space Complexity: O(1)
-    int right = count(a, end);
-    int left = count(a, start - 1);
-    return right - left;
-  }
+	public static int countInRange(int[] a, int start, int end) {
+		// Time Complexity: O(n)
+		// Space Complexity: O(1)
+		int right = count(a, end);
+		int left = count(a, start - 1);
+		return right - left;
+	}
 
-  public static void main(String[] args) {
+	private static int count(int[] a, int k) {
+		int n = a.length;
+		int sum = 0;
+		int count = 0;
+		int left = 0;
 
-    int[] a1 = { 1, 4, 6 };
-    System.out.println(countInRange(a1, 3, 8));
+		for (int right = 0; right < n; right++) {
+			sum += a[right];
+			if (sum < k) {
+				right++;
+				count += (right - left);
+			} else {
+				sum -= a[left];
+				left++;
+			}
+		}
+		return count;
+	}
 
-    int[] a2 = { 2, 3, 5, 8 };
-    System.out.println(countInRange(a2, 4, 13));
+	public static void main(String[] args) {
 
-  }
+		int[] a1 = { 1, 4, 6 };
+		System.out.println(countInRange(a1, 3, 8));
+
+		int[] a2 = { 2, 3, 5, 8 };
+		System.out.println(countInRange(a2, 4, 13));
+
+	}
 }
