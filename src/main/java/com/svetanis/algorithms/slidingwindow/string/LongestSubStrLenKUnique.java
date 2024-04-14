74ec8f62e77cd75fe0e287b3adfd4328ae3e5a8f
svetanis <svet125@gmail.com>
2024-04-14 14:24:49 -0700

    longest substr length with K unique chars

--- src/main/java/com/svetanis/algorithms/slidingwindow/string/LongestSubStrLenKUnique.java
+++ src/main/java/com/svetanis/algorithms/slidingwindow/string/LongestSubStrLenKUnique.java
@@ -6,12 +6,12 @@ import static java.lang.Math.max;
 
 import java.util.Map;
 
-// Given a string you need to print longest possible substring 
-// that has exactly K unique characters. 
+// Given a string, find the length of the longest 
+// substring with no more than K unique characters 
 
 public final class LongestSubStrLenKUnique {
 
-  public static int kUnique(String str, int k) {
+  public static int kUniqueMaxLen(String str, int k) {
     // Time complexity: O(n)
 
     int n = str.length();
@@ -20,39 +20,49 @@ public final class LongestSubStrLenKUnique {
     }
 
     int left = 0; // current start
-    int right = 0; // current end
     int max = 0; // max window size
     Map<Character, Integer> map = newHashMap();
-    while (right < n) {
+    for (int right = 0; right < n; right++) {
       char next = str.charAt(right);
-      map.put(next, map.getOrDefault(next, 0) + 1);
-      
+      int freq = map.getOrDefault(next, 0) + 1;
+      map.put(next, freq);
+      // shrink the sliding window, until k
+      // distinct chars left in frequency map
       while (map.size() > k) {
         char front = str.charAt(left);
         map.put(front, map.get(front) - 1);
         if (map.get(front) == 0) {
           map.remove(front);
         }
-        left++;
+        left++; // shrink the window
       }
+      // max length so far
       if (map.size() == k) {
         max = max(max, right - left + 1);
       }
-      right++;
     }
     return max;
   }
 
   public static void main(String[] args) {
     String str1 = "aabbcc";
-    System.out.println(kUnique(str1, 1));
-    System.out.println(kUnique(str1, 2));
-    System.out.println(kUnique(str1, 3));
+    System.out.println(kUniqueMaxLen(str1, 1));
+    System.out.println(kUniqueMaxLen(str1, 2));
+    System.out.println(kUniqueMaxLen(str1, 3));
 
     String str2 = "aaabbb";
-    System.out.println(kUnique(str2, 3));
+    System.out.println(kUniqueMaxLen(str2, 3));
 
     String str3 = "aabacbebebe";
-    System.out.println(kUnique(str3, 3));
+    System.out.println(kUniqueMaxLen(str3, 3));
+
+    String str4 = "araaci";
+    System.out.println(kUniqueMaxLen(str4, 2));
+
+    String str5 = "araaci";
+    System.out.println(kUniqueMaxLen(str5, 1));
+    
+    String str6 = "cbbebi";
+    System.out.println(kUniqueMaxLen(str6, 3));
   }
 }
