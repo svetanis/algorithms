8d1cfd1945f50daec85fa6d4b2ce8fb281ea0f9b
svetanis <svet125@gmail.com>
2024-04-21 16:59:43 -0700

    min window substr

--- src/main/java/com/svetanis/algorithms/slidingwindow/string/minwindow/MinWindowSubStrLen.java
+++ src/main/java/com/svetanis/algorithms/slidingwindow/string/minwindow/MinWindowSubStrLen.java
@@ -6,8 +6,14 @@ import static java.lang.Math.min;
 
 import java.util.Map;
 
-public final class MinWindowSubStrLen {
+// given a string and a pattern, find the smallest substring
+// in the given string which has all the chars of the given pattern
+// the required substring can have some additional characters
+// and doesn't need to be a permutation of the pattern.
 
+public final class MinWindowSubStrLen {
+  // Time complexity: O(n + m)	
+	
   public static int minWindow(String s, String p) {
     int n = s.length();
     int m = p.length();
@@ -18,6 +24,8 @@ public final class MinWindowSubStrLen {
     Map<Character, Integer> map = freqMap(p);
 
     for (int right = 0; right < n; right++) {
+      // 1. keep a running count of every 
+      // matching instance of a char	
       char c = s.charAt(right);
       if (map.containsKey(c)) {
         map.put(c, map.get(c) - 1);
@@ -26,14 +34,25 @@ public final class MinWindowSubStrLen {
         }
       }
     
-      // shrink window
+      // 2. whenever all the chars are matched, 
+      // shrink the window from the beginning
       while (matched == m) {
-        min = min(min, right - left + 1);
+        // keep track of the smallest substr
+    	// that has all the matching chars
+    	min = min(min, right - left + 1);
+    	// 3. stop the shrinking process as soon as
+    	// a matched char removed from sliding window 
         char front = s.charAt(left++);
+        // redundant matching char (aa)
+        // when need only one
         if (map.containsKey(front)) {
+          // second redundant: decrement the matched count  
+          // when second char goes out of the window	
           if (map.get(front) == 0) {
             matched--;
           }
+          // first redundant: shrink window without
+          // decrementing the matched count
           map.put(front, map.get(front) + 1);
         }
       }
