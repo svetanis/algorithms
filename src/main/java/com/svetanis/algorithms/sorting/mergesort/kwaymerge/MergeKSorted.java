f08b0deee00616c5007f0b3e4d69f13fe82ac2a8
svetanis <svet125@gmail.com>
2024-08-14 15:03:00 -0700

    merge k sorted

--- src/main/java/com/svetanis/algorithms/sorting/mergesort/kwaymerge/MergeKSorted.java
+++ src/main/java/com/svetanis/algorithms/sorting/mergesort/kwaymerge/MergeKSorted.java
@@ -3,6 +3,7 @@ package com.svetanis.algorithms.sorting.mergesort.kwaymerge;
 import static com.google.common.collect.Lists.newArrayList;
 import static com.svetanis.java.base.collect.Lists.newList;
 import static com.svetanis.java.base.utils.Print.print;
+import static java.util.Arrays.asList;
 
 import java.util.List;
 import java.util.PriorityQueue;
@@ -21,21 +22,21 @@ import com.google.common.collect.ImmutableList;
 public final class MergeKSorted {
 	// Time Complexity: O(n*k log k)
 
-	public static ImmutableList<Integer> merge(int[][] a) {
-		Queue<Node> pq = init(a);
-		return merge(a, pq);
+	public static ImmutableList<Integer> merge(List<List<Integer>> lists) {
+		Queue<Node> pq = init(lists);
+		return merge(lists, pq);
 	}
 
-	private static Queue<Node> init(int[][] a) {
+	private static Queue<Node> init(List<List<Integer>> lists) {
 		Queue<Node> pq = new PriorityQueue<>();
-		for (int i = 0; i < a.length; i++) {
-			Node node = new Node(a[i][0], i, 1);
-			pq.offer(node);
+		for (int i = 0; i < lists.size(); i++) {
+			int val = lists.get(i).get(0);
+			pq.offer(new Node(val, i, 1));
 		}
 		return pq;
 	}
 
-	private static ImmutableList<Integer> merge(int[][] a, Queue<Node> pq) {
+	private static ImmutableList<Integer> merge(List<List<Integer>> lists, Queue<Node> pq) {
 		// one by one get the min element
 		// from min heap and replace it
 		// with next element of its array
@@ -49,8 +50,9 @@ public final class MergeKSorted {
 			// replace the current root of heap
 			// the next element belongs to the
 			// same array as the current root
-			if (node.next < a[node.id].length) {
-				int val = a[node.id][node.next];
+			int size = lists.get(node.id).size();
+			if (node.next < size) {
+				int val = lists.get(node.id).get(node.next);
 				int next = node.next + 1;
 				pq.offer(new Node(val, node.id, next));
 			}
@@ -59,7 +61,10 @@ public final class MergeKSorted {
 	}
 
 	public static void main(String[] args) {
-		int[][] a = { { 2, 6, 12 }, { 1, 9 }, { 23, 34, 90, 2000 } };
-		print(merge(a));
+		List<List<Integer>> lists = newArrayList();
+		lists.add(asList(2, 6, 12));
+		lists.add(asList(1, 9));
+		lists.add(asList(23, 34, 90, 2000));
+		print(merge(lists));
 	}
 }
