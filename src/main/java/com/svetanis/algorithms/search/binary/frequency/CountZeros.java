147863ed07a9dad88d09ddf54f12ae610b1a7bdc
svetanis <svet125@gmail.com>
2024-08-17 15:03:41 -0700

    count zeros

--- src/main/java/com/svetanis/algorithms/search/binary/frequency/CountZeros.java
+++ src/main/java/com/svetanis/algorithms/search/binary/frequency/CountZeros.java
@@ -1,25 +1,34 @@
 package com.svetanis.algorithms.search.binary.frequency;
 
+import static com.google.common.base.Optional.absent;
+import static com.google.common.base.Optional.of;
+
+import com.google.common.base.Optional;
+
+// given an array of 1s and 0s which has 
+// all 1s first followed by all 0s
+// count the number of zeros in the given array
+
 public final class CountZeros {
 
   public static int countZeros(int[] a) {
     int n = a.length;
-    int first = firstZero(a, 0, n - 1);
-    if (first == -1) {
-      return 0;
+    Optional<Integer> first = firstZero(a, 0, n - 1);
+    if (first.isPresent()) {
+      return n - first.get();
     }
-    return n - first;
+    return 0;
   }
 
-  private static int firstZero(int[] a, int left, int right) {
+  private static Optional<Integer> firstZero(int[] a, int left, int right) {
     // O(log n)
 
     if (right < left) {
-      return -1;
+      return absent();
     }
     int mid = left + (right - left) / 2;
     if ((mid == 0 || a[mid - 1] == 1) && a[mid] == 0) {
-      return mid;
+      return of(mid);
     }
     // if mid element is not 0
     if (a[mid] == 1) {
