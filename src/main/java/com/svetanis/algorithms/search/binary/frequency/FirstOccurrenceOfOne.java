533b15e33e608d2a4d7d297ed5c740270b5a038a
svetanis <svet125@gmail.com>
2024-08-17 14:56:55 -0700

    first occurrence of one

--- src/main/java/com/svetanis/algorithms/search/binary/frequency/FirstOccurrenceOfOne.java
+++ src/main/java/com/svetanis/algorithms/search/binary/frequency/FirstOccurrenceOfOne.java
@@ -1,29 +1,34 @@
 package com.svetanis.algorithms.search.binary.frequency;
 
-import static java.util.Arrays.sort;
+import static com.google.common.base.Optional.absent;
+import static com.google.common.base.Optional.of;
+
+import com.google.common.base.Optional;
+
+// given a sorted array consisting of 0's and 1's
+// find the index of first '1' in the sorted array
 
 public final class FirstOccurrenceOfOne {
 
-  public static int firstOccurrence(int[] a) {
+  public static Optional<Integer> firstOccurrence(int[] a) {
     int n = a.length;
-    sort(a);
     return firstOccurrence(a, 0, n - 1);
   }
 
-  public static int firstOccurrence(int[] a, int left, int right) {
+  public static Optional<Integer> firstOccurrence(int[] a, int left, int right) {
     // O(log n)
 
     while (left <= right) {
       int mid = left + (right - left) / 2;
       if ((mid == 0 || a[mid - 1] == 0) && a[mid] == 1) {
-        return mid;
+        return of(mid);
       } else if (a[mid] == 1) {
         right = mid - 1;
       } else {
         left = mid + 1;
       }
     }
-    return -1;
+    return absent();
   }
 
   public static void main(String[] args) {
