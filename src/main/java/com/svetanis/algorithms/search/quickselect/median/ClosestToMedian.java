e8b0ef3080a577b7008b60b9afa9ac502bd6a453
svetanis <svet125@gmail.com>
2024-08-25 15:04:34 -0700

    closest to median

--- src/main/java/com/svetanis/algorithms/search/quickselect/median/ClosestToMedian.java
+++ src/main/java/com/svetanis/algorithms/search/quickselect/median/ClosestToMedian.java
@@ -9,38 +9,43 @@ import java.util.Queue;
 
 import com.google.common.collect.ImmutableList;
 
-// Design an O(n) time algorithm to compute the k elements closest to the median of an array A.
+// Design an O(n) time algorithm to compute 
+// the k elements closest to the median of an array A.
 
 public final class ClosestToMedian {
 
-  public static ImmutableList<Integer> closestToMedian(int[] a, int k) {
-    int n = a.length;
-    int median = minSelect(a, n / 2);
-    return closest(a, k, median);
-  }
+	public static ImmutableList<Integer> closestToMedian(int[] a, int k) {
+		// Time Complexity: O(n * log k)
+		
+		int median = minSelect(a);
+		return closest(a, k, median);
+	}
 
-  private static ImmutableList<Integer> closest(int[] a, int k, int median) {
-    int n = a.length;
-    Queue<Integer> pq = init(a, k);
-    for (int i = k; i < n; ++i) {
-      if (abs(median - pq.peek()) > abs(median - a[i])) {
-        pq.poll();
-        pq.offer(a[i]);
-      }
-    }
-    return newList(pq);
-  }
+	private static ImmutableList<Integer> closest(int[] a, int k, int median) {
+		Queue<Integer> pq = priorityQueue(a, k);
+		for (int i = k; i < a.length; ++i) {
+			int dist1 = abs(median - pq.peek());
+			int dist2 = abs(median - a[i]);
+			if (dist1 > dist2) {
+				pq.poll();
+				pq.add(a[i]);
+			}
+		}
+		return newList(pq);
+	}
 
-  private static Queue<Integer> init(int[] a, int k) {
-    Queue<Integer> pq = new PriorityQueue<>();
-    for (int i = 0; i < k; ++i) {
-      pq.offer(a[i]);
-    }
-    return pq;
-  }
+	private static Queue<Integer> priorityQueue(int[] a, int k) {
+		Queue<Integer> pq = new PriorityQueue<>((x, y) -> x - y);
+		for (int i = 0; i < k; ++i) {
+			pq.offer(a[i]);
+		}
+		return pq;
+	}
 
-  public static void main(String[] args) {
-    int[] a = { 7, 14, 10, 12, 2, 11, 29, 3, 4 };
-    System.out.println(closestToMedian(a, 5));
-  }
+	public static void main(String[] args) {
+		int[] a = { 7, 14, 10, 12, 2, 11, 29, 3, 4 };
+		System.out.println(closestToMedian(a, 5));
+		// median == 10
+		// 7, 10, 14, 11, 12
+	}
 }
--- src/main/java/com/svetanis/algorithms/search/quickselect/median/MedianUnsortedMinSelect.java
+++ src/main/java/com/svetanis/algorithms/search/quickselect/median/MedianUnsortedMinSelect.java
@@ -4,31 +4,30 @@ import static com.svetanis.java.base.utils.Swap.swap;
 
 public final class MedianUnsortedMinSelect {
 
-  public static int minSelect(int[] a, int k) {
-    // time complexity: O(kn)
-    int n = a.length;
+	public static int minSelect(int[] a) {
+		// time complexity: O(k * n)
+		
+		int n = a.length;
+		int k = n / 2;
 
-    for (int i = 0; i < k; ++i) {
-      int index = i;
-      for (int j = i + 1; j < n; ++j) {
-        index = j;
-      }
-      swap(a, i, index);
-    }
-    return a[k - 1];
-  }
+		for (int i = 0; i < k; ++i) {
+			int index = i;
+			for (int j = i + 1; j < n; ++j) {
+				index = j;
+			}
+			swap(a, i, index);
+		}
+		return a[k - 1];
+	}
 
-  public static void main(String[] args) {
-    int[] a = { 7, 14, 10, 12, 2, 11, 29, 3, 4 };
-    int n = a.length;
-    System.out.println("median: " + minSelect(a, n / 2));
+	public static void main(String[] args) {
+		int[] a = { 7, 14, 10, 12, 2, 11, 29, 3, 4 };
+		System.out.println("median: " + minSelect(a));
 
-    int[] a1 = { 0, 1, 2, 3, -2, 4, -4 };
-    int m = a1.length;
-    System.out.println("median: " + minSelect(a1, m / 2));
+		int[] a1 = { 0, 1, 2, 3, -2, 4, -4 };
+		System.out.println("median: " + minSelect(a1));
 
-    int[] a2 = { 6, 7, 8, 1, 2, 3, 4, 5, 9, 10 };
-    int k = a2.length;
-    System.out.println("median: " + minSelect(a2, k / 2));
-  }
+		int[] a2 = { 6, 7, 8, 1, 2, 3, 4, 5, 9, 10 };
+		System.out.println("median: " + minSelect(a2));
+	}
 }
