1013515f37ae5d4ee058550b8b283669d65d9c03
svetanis <svet125@gmail.com>
2024-09-05 11:32:32 -0700

    generalize abbreviations

---
+++ src/main/java/com/svetanis/algorithms/recursive/GeneralizeAbbreviationsRecursive.java
@@ -0,0 +1,49 @@
+package com.svetanis.algorithms.recursive;
+
+import static com.google.common.collect.Lists.newArrayList;
+import static com.svetanis.java.base.collect.Lists.newList;
+
+import java.util.List;
+
+import com.google.common.collect.ImmutableList;
+
+// given a word, generate all of its
+// unique generalized abbreviations
+
+public final class GeneralizeAbbreviationsRecursive {
+	// Time Complexity: O(n * 2^n)
+	// Space Complexity: O(n * 2^n)
+
+	public static ImmutableList<String> generate(String s) {
+		List<String> list = newArrayList();
+		generate(s, new StringBuilder(), 0, 0, list);
+		return newList(list);
+	}
+
+	private static void generate(String s, StringBuilder sb, int start, int count, List<String> list) {
+
+		if (start == s.length()) {
+			if (count != 0) {
+				sb.append(count);
+			}
+			list.add(sb.toString());
+			return;
+		}
+
+		// continue abbreviation
+		StringBuilder sbc = new StringBuilder(sb);
+		generate(s, sbc, start + 1, count + 1, list);
+		// restart abbreviation
+		if (count != 0) {
+			sb.append(count);
+		}
+		StringBuilder sbr = new StringBuilder(sb);
+		sbr.append(s.charAt(start));
+		generate(s, sbr, start + 1, 0, list);
+	}
+
+	public static void main(String[] args) {
+		System.out.println(generate("BAT"));
+		System.out.println(generate("code"));
+	}
+}
---
+++ src/main/java/com/svetanis/algorithms/recursive/GeneralizedAbbreviationsIterative.java
@@ -0,0 +1,66 @@
+package com.svetanis.algorithms.recursive;
+
+import static com.google.common.collect.Lists.newArrayList;
+import static com.google.common.collect.Lists.newLinkedList;
+import static com.svetanis.java.base.collect.Lists.newList;
+
+import java.util.List;
+import java.util.Queue;
+
+import com.google.common.collect.ImmutableList;
+
+// given a word, generate all of its
+// unique generalized abbreviations
+
+public final class GeneralizedAbbreviationsIterative {
+	// Time Complexity: O(n * 2^n)
+	// Space Complexity: O(n * 2^n)
+
+	public static ImmutableList<String> generate(String s) {
+		List<String> list = newArrayList();
+		Queue<Abbreviation> queue = newLinkedList();
+		queue.add(new Abbreviation(new StringBuilder(), 0, 0));
+		while (!queue.isEmpty()) {
+			Abbreviation abb = queue.poll();
+			if (abb.start == s.length()) {
+				if (abb.count != 0) {
+					abb.sb.append(abb.count);
+				}
+				list.add(abb.sb.toString());
+			} else {
+				// continue abbreviation
+				StringBuilder sbc = new StringBuilder(abb.sb);
+				int start = abb.start + 1;
+				int count = abb.count + 1;
+				queue.add(new Abbreviation(sbc, start, count));
+
+				// restart abbreviation
+				if (abb.count != 0) {
+					abb.sb.append(abb.count);
+				}
+				StringBuilder sbr = new StringBuilder(abb.sb);
+				sbr.append(s.charAt(abb.start));
+				int start2 = abb.start + 1;
+				queue.add(new Abbreviation(sbr, start2, 0));
+			}
+		}
+		return newList(list);
+	}
+
+	public static void main(String[] args) {
+		System.out.println(generate("BAT"));
+		System.out.println(generate("code"));
+	}
+
+	private static final class Abbreviation {
+		private StringBuilder sb;
+		private int start;
+		private int count;
+
+		public Abbreviation(StringBuilder sb, int start, int count) {
+			this.sb = sb;
+			this.start = start;
+			this.count = count;
+		}
+	}
+}
