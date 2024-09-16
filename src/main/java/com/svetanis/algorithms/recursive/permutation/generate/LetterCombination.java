bd8d2ce43b8086e337fc65e176945f9a319f8b6c
svetanis <svet125@gmail.com>
2024-09-16 11:44:03 -0700

    backtracking: letter combination

---
+++ src/main/java/com/svetanis/algorithms/recursive/permutation/generate/LetterCombination.java
@@ -0,0 +1,51 @@
+package com.svetanis.algorithms.recursive.permutation.generate;
+
+import static com.google.common.collect.Lists.newArrayList;
+import static com.svetanis.java.base.collect.Lists.newList;
+import static com.svetanis.java.base.utils.Print.printLines;
+import static java.util.Arrays.asList;
+
+import java.util.List;
+
+import com.google.common.base.Joiner;
+import com.google.common.collect.ImmutableList;
+
+// given a positive integer number n 
+// find all n-letter words composed 
+// by 'a' and 'b' and return them in
+// a list of string in lexicographical order
+
+public final class LetterCombination {
+	// Time Complexity: O(n * 2^n)
+	// Space Complexity: O(n * 2^n)
+
+	private static final ImmutableList<String> DICT = newList(asList("a", "b"));
+
+	public static ImmutableList<String> letterCombination(int n) {
+		List<String> list = newArrayList();
+		List<String> lists = newArrayList();
+		dfs(n, list, lists);
+		return newList(lists);
+	}
+
+	private static void dfs(int n, List<String> list, List<String> lists) {
+		if (list.size() == n) {
+			lists.add(Joiner.on("").join(list));
+			return;
+		}
+
+		for (String letter : DICT) {
+			list.add(letter);
+			dfs(n, list, lists);
+			list.remove(list.size() - 1);
+		}
+	}
+
+	public static void main(String[] args) {
+		printLines(letterCombination(2)); // aa, ab, ba, bb
+		System.out.println();
+		printLines(letterCombination(3));
+		System.out.println();
+		printLines(letterCombination(4));
+	}
+}
