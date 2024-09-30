eac0b945c85f2ae4bef842a4f020e32c1095ed23
svetanis <svet125@gmail.com>
2024-09-30 12:01:27 -0700

    smallest subarray given sum

--- src/main/java/com/svetanis/algorithms/slidingwindow/array/MinSizeSubArraySum.java
+++ src/main/java/com/svetanis/algorithms/slidingwindow/array/MinSizeSubArraySum.java
@@ -9,34 +9,32 @@ import static java.lang.Math.min;
 // Return 0 if no such subarray exists.
 
 public final class MinSizeSubArraySum {
+	// Time complexity: O(n)
 
-  public static int minSum(int[] a, int k) {
-    // Time complexity: O(n)
+	public static int minSum(int[] a, int target) {
+		int n = a.length;
+		int sum = 0;
+		int left = 0;
+		int min = MAX_VALUE;
+		for (int right = 0; right < n; right++) {
+			sum += a[right];
+			while (sum >= target && left < n) {
+				min = min(min, right - left + 1);
+				sum -= a[left];
+				left++;
+			}
+		}
+		return min == MAX_VALUE ? 0 : min;
+	}
 
-    int n = a.length;
-    int min = MAX_VALUE;
-    int sum = 0;
-    int left = 0;
+	public static void main(String[] args) {
+		int[] ar1 = { 1, 4, 45, 6, 0, 19 };
+		System.out.println(minSum(ar1, 51));
 
-    for (int right = 0; right < n; right++) {
-      sum += a[right];
-      while (sum >= k && left < n) {
-        min = min(min, right - left + 1);
-        sum -= a[left];
-        left++;
-      }
-    }
-    return min == MAX_VALUE ? 0 : min;
-  }
+		int[] ar2 = { 1, 10, 5, 2, 7 };
+		System.out.println(minSum(ar2, 9));
 
-  public static void main(String[] args) {
-    int[] ar1 = { 1, 4, 45, 6, 0, 19 };
-    System.out.println(minSum(ar1, 51));
-
-    int[] ar2 = { 1, 10, 5, 2, 7 };
-    System.out.println(minSum(ar2, 9));
-
-    int[] ar3 = { 1, 11, 100, 1, 0, 200, 3, 2, 1, 250 };
-    System.out.println(minSum(ar3, 280));
-  }
+		int[] ar3 = { 1, 11, 100, 1, 0, 200, 3, 2, 1, 250 };
+		System.out.println(minSum(ar3, 280));
+	}
 }
