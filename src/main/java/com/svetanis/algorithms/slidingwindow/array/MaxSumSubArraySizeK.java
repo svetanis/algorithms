9bbdf5f162911ea5b4a11b4c9ba2f2aacf7679cc
svetanis <svet125@gmail.com>
2024-09-30 11:39:55 -0700

    max sum distinct subarray of size k

--- src/main/java/com/svetanis/algorithms/slidingwindow/array/MaxSumSubArraySizeK.java
+++ src/main/java/com/svetanis/algorithms/slidingwindow/array/MaxSumSubArraySizeK.java
@@ -1,35 +1,42 @@
 package com.svetanis.algorithms.slidingwindow.array;
 
-import static com.svetanis.java.base.utils.Arrays.sum;
 import static java.lang.Math.max;
+import static java.util.Arrays.asList;
+
+import java.util.List;
 
 // Given an array of positive numbers and a positive number ‘k’, 
 // find the maximum sum of any contiguous subarray of size ‘k’.
 
 public final class MaxSumSubArraySizeK {
+	// Time Complexity: O(n)
+	// Aux Space: O(1)
 
-  public static int maxSum(int[] a, int k) {
-    // Time Complexity: O(n)
-    // Aux Space: O(1)
-    
-    int n = a.length;
-    if (n < k) {
-      return -1;
-    }
+	public static int maxSum(int k, List<Integer> list) {
+		int n = list.size();
+		if (n < k) {
+			return -1;
+		}
+		int max = sum(list, k);
+		int sum = max;
+		for (int i = k; i < n; i++) {
+			sum += list.get(i) - list.get(i - k);
+			max = max(max, sum);
+		}
+		return max;
+	}
 
-    int max = sum(a, k);
+	private static int sum(List<Integer> list, int k) {
+		int sum = 0;
+		for (int i = 0; i < k; i++) {
+			sum += list.get(i);
+		}
+		return sum;
+	}
 
-    int sum = max;
-    for (int i = k; i < n; i++) {
-      sum += a[i] - a[i - k];
-      max = max(max, sum);
-    }
-    return max;
-  }
-
-  public static void main(String[] args) {
-    int[] a = { 1, 4, 2, 10, 2, 3, 1, 0, 20 };
-    System.out.println(maxSum(a, 4));
-  }
+	public static void main(String[] args) {
+		System.out.println(maxSum(4, asList(1, 4, 2, 10, 2, 3, 1, 0, 20))); // 24
+		System.out.println(maxSum(3, asList(2, 1, 5, 1, 3, 2))); // 9
+		System.out.println(maxSum(2, asList(2, 3, 4, 1, 5))); // 7
+	}
 }
-
