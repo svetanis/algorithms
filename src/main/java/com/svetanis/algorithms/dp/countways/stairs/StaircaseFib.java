e6568064c626b11dfa7186b4044dc2327d2f2c9e
svetanis <svet125@gmail.com>
2024-09-30 18:12:40 -0700

    count stairs

--- src/main/java/com/svetanis/algorithms/dp/countways/stairs/StaircaseFib.java
+++ src/main/java/com/svetanis/algorithms/dp/countways/stairs/StaircaseFib.java
@@ -2,38 +2,35 @@ package com.svetanis.algorithms.dp.countways.stairs;
 
 public final class StaircaseFib {
 
-  // Given a stair with ‘n’ steps,
-  // implement a method to count
-  // how many possible ways are there
-  // to reach the top of the staircase,
-  // given that, at every step you can
-  // either take 1 step, 2 steps, or 3 steps.
+	// Given a stair with ‘n’ steps,
+	// implement a method to count
+	// how many possible ways are there
+	// to reach the top of the staircase,
+	// given that, at every step you can
+	// either take 1 or 2 stairs.
 
-  public static int countWays(int n) {
-    return fibOptimal(n + 1);
-  }
+	public static int countWays(int n) {
+		return fibOptimal(n + 1);
+	}
 
-  public static int fibOptimal(int n) {
+	public static int fibOptimal(int n) {
+		if (n == 0) {
+			return 0;
+		}
+		if (n == 1) {
+			return 1;
+		}
+		int fib1 = 0;
+		int fib2 = 1;
+		for (int i = 2; i <= n; i++) {
+			int fib = fib1 + fib2;
+			fib1 = fib2;
+			fib2 = fib;
+		}
+		return fib2;
+	}
 
-    if (n == 0) {
-      return 0;
-    }
-
-    if (n == 1) {
-      return 1;
-    }
-
-    int fib1 = 0;
-    int fib2 = 1;
-    for (int i = 2; i <= n; i++) {
-      int fib = fib1 + fib2;
-      fib1 = fib2;
-      fib2 = fib;
-    }
-    return fib2;
-  }
-
-  public static void main(String[] args) {
-    System.out.println(countWays(4));
-  }
+	public static void main(String[] args) {
+		System.out.println(countWays(4));
+	}
 }
