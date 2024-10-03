48c199f9d35d237812148fa8c06895d706e3aefc
svetanis <svet125@gmail.com>
2024-10-02 20:46:43 -0700

    shortest subarray with sum >= target

--- src/main/java/com/svetanis/algorithms/slidingwindow/array/MinSizeSubArraySum.java
+++ src/main/java/com/svetanis/algorithms/slidingwindow/array/MinSizeSubArraySum.java
@@ -1,7 +1,9 @@
 package com.svetanis.algorithms.slidingwindow.array;
 
-import static java.lang.Integer.MAX_VALUE;
 import static java.lang.Math.min;
+import static java.util.Arrays.asList;
+
+import java.util.List;
 
 // Given an array of positive integers and a number ‘S,’ 
 // find the length of the smallest contiguous subarray 
@@ -11,30 +13,26 @@ import static java.lang.Math.min;
 public final class MinSizeSubArraySum {
 	// Time complexity: O(n)
 
-	public static int minSum(int[] a, int target) {
-		int n = a.length;
+	public static int minSum(int target, List<Integer> list) {
 		int sum = 0;
 		int left = 0;
-		int min = MAX_VALUE;
+		int n = list.size();
+		int min = n;
 		for (int right = 0; right < n; right++) {
-			sum += a[right];
+			sum += list.get(right);
 			while (sum >= target && left < n) {
 				min = min(min, right - left + 1);
-				sum -= a[left];
+				sum -= list.get(left);
 				left++;
 			}
 		}
-		return min == MAX_VALUE ? 0 : min;
+		return min;
 	}
 
 	public static void main(String[] args) {
-		int[] ar1 = { 1, 4, 45, 6, 0, 19 };
-		System.out.println(minSum(ar1, 51));
-
-		int[] ar2 = { 1, 10, 5, 2, 7 };
-		System.out.println(minSum(ar2, 9));
-
-		int[] ar3 = { 1, 11, 100, 1, 0, 200, 3, 2, 1, 250 };
-		System.out.println(minSum(ar3, 280));
+		System.out.println(minSum(51, asList(1, 4, 45, 6, 0, 19)));
+		System.out.println(minSum(9, asList(1, 10, 5, 2, 7)));
+		System.out.println(minSum(280, asList(1, 11, 100, 1, 0, 200, 3, 2, 1, 250)));
+		System.out.println(minSum(10, asList(1, 4, 1, 7, 3, 0, 2, 5))); // 2
 	}
 }
