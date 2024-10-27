94f022b964a73e8749199c29472ade6914f16277
svetanis <svet125@gmail.com>
2024-10-26 22:24:55 -0700

    disjoint intervals

---
+++ src/main/java/com/svetanis/algorithms/sorting/mergesort/interval/DisjointIntervals.java
@@ -0,0 +1,67 @@
+package com.svetanis.algorithms.sorting.mergesort.interval;
+
+import static com.svetanis.java.base.collect.Lists.newList;
+import static com.svetanis.java.base.utils.Print.print;
+import static java.lang.Math.max;
+import static java.lang.Math.min;
+
+import java.util.TreeMap;
+
+import com.google.common.collect.ImmutableList;
+
+// 352. Data Stream as Disjoint Intervals
+
+public final class DisjointIntervals {
+	// Time Complexity: O(n)
+
+	public DisjointIntervals() {
+		this.map = new TreeMap<>();
+	}
+
+	private TreeMap<Integer, Interval> map;
+
+	public void addNum(int value) {
+		// interval immediately before the new value
+		Integer left = map.floorKey(value);
+		// interval immediately after the new value
+		Integer right = map.ceilingKey(value);
+		boolean notNull = left != null && right != null;
+		if (notNull && map.get(left).end + 1 == value && map.get(right).start - 1 == value) {
+			// merge both intervals
+			map.get(left).end = map.get(right).end;
+			// remove the redundant interval
+			map.remove(right);
+		} else if (left != null && value <= map.get(left).end + 1) {
+			// extend the interval to the left
+			map.get(left).end = max(value, map.get(left).end);
+		} else if (right != null && value >= map.get(right).start - 1) {
+			// extend the interval to the right
+			map.get(right).start = min(value, map.get(right).start);
+		} else {
+			Interval interval = new Interval(value, value);
+			map.put(value, interval);
+		}
+	}
+
+	public ImmutableList<Interval> getIntervals() {
+		return newList(map.values());
+	}
+
+	public static void main(String[] args) {
+		DisjointIntervals srs = new DisjointIntervals();
+		srs.addNum(1); // 1
+		print(srs.getIntervals()); // [[1,1]]
+
+		srs.addNum(3); // 1, 3
+		print(srs.getIntervals()); // [[1,1], [3,3]]
+
+		srs.addNum(7); // 1, 3, 7
+		print(srs.getIntervals()); // [[1,1], [3,3], [7,7]]
+
+		srs.addNum(2); // 1, 2, 3, 7
+		print(srs.getIntervals()); // [[1,3], [7,7]]
+
+		srs.addNum(6); // 1, 2, 3, 6, 7
+		print(srs.getIntervals()); // [[1,3], [6,7]]
+	}
+}
---
+++ src/main/java/com/svetanis/algorithms/sorting/mergesort/interval/DisjointIntervalsSubmit.java
@@ -0,0 +1,68 @@
+package com.svetanis.algorithms.sorting.mergesort.interval;
+
+import static com.svetanis.java.base.utils.Print.print;
+
+import java.util.TreeMap;
+
+// 352. Data Stream as Disjoint Intervals
+
+public final class DisjointIntervalsSubmit {
+	// Time Complexity: O(n)
+
+	public DisjointIntervalsSubmit() {
+		this.map = new TreeMap<>();
+	}
+
+	private TreeMap<Integer, int[]> map;
+
+	public void addNum(int value) {
+		// interval immediately before the new value
+		Integer left = map.floorKey(value);
+		// interval immediately after the new value
+		Integer right = map.ceilingKey(value);
+		boolean notNull = left != null && right != null;
+		if (notNull && map.get(left)[1] + 1 == value && map.get(right)[0] - 1 == value) {
+			// merge both intervals
+			map.get(left)[1] = map.get(right)[1];
+			// remove the redundant interval
+			map.remove(right);
+		} else if (left != null && value <= map.get(left)[1] + 1) {
+			// extend the interval to the left
+			map.get(left)[1] = Math.max(value, map.get(left)[1]);
+		} else if (right != null && value >= map.get(right)[0] - 1) {
+			// extend the interval to the right
+			map.get(right)[0] = Math.min(value, map.get(right)[0]);
+		} else {
+			int[] interval = { value, value };
+			map.put(value, interval);
+		}
+	}
+
+	public int[][] getIntervals() {
+		int[][] intervals = new int[map.size()][2];
+		int i = 0;
+		for (int[] interval : map.values()) {
+			intervals[i++] = interval;
+		}
+		return intervals;
+	}
+
+	public static void main(String[] args) {
+		DisjointIntervalsSubmit srs = new DisjointIntervalsSubmit();
+		srs.addNum(1); // 1
+		print(srs.getIntervals()); // [[1,1]]
+
+		srs.addNum(3); // 1, 3
+		print(srs.getIntervals()); // [[1,1], [3,3]]
+
+		srs.addNum(7); // 1, 3, 7
+		print(srs.getIntervals()); // [[1,1], [3,3], [7,7]]
+
+		srs.addNum(2); // 1, 2, 3, 7
+		print(srs.getIntervals()); // [[1,3], [7,7]]
+
+		srs.addNum(6); // 1, 2, 3, 6, 7
+		print(srs.getIntervals()); // [[1,3], [6,7]]
+
+	}
+}
