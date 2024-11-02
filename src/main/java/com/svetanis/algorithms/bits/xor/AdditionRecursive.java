abda6c0e74636dd19962bab646de55472647a3c0
svetanis <svet125@gmail.com>
2024-11-01 19:38:37 -0700

    sum of two integers

---
+++ src/main/java/com/svetanis/algorithms/bits/xor/AdditionRecursive.java
@@ -0,0 +1,18 @@
+package com.svetanis.algorithms.bits.xor;
+
+public final class AdditionRecursive {
+
+  public static int add(int a, int b) {
+    if (b == 0) {
+      return a;
+    }
+    int sum = a ^ b; // add without carrying
+    int carry = (a & b) << 1; // carry but don't add
+    return add(sum, carry); // recurse
+  }
+
+  public static void main(String[] args) {
+    System.out.println(add(5, 11));
+  }
+}
+
