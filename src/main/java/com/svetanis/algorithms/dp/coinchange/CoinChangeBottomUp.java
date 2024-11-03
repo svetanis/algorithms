77975d99640c96ae10734dd02a3f3ec07c58d354
svetanis <svet125@gmail.com>
2024-11-02 23:35:48 -0700

    dp: coin change

--- src/main/java/com/svetanis/algorithms/dp/coinchange/CoinChangeBottomUp.java
+++ src/main/java/com/svetanis/algorithms/dp/coinchange/CoinChangeBottomUp.java
@@ -1,42 +1,44 @@
 package com.svetanis.algorithms.dp.coinchange;
 
+import static java.util.Arrays.asList;
 import static java.util.Arrays.fill;
+
+import java.util.List;
+
+// n - size of array of coins S
+// V - coin value
+// we need n + 1 rows as the table is
+// constructed in bottom up manner
+// using the base case 0 value case (n = 0)
 
 public final class CoinChangeBottomUp {
 
-  // n - size of array of coins S
-  // V - coin value
-  // we need n + 1 rows as the table is
-  // constructed in bottom up manner
-  // using the base case 0 value case (n = 0)
-  public static int change(int[] a, int max) {
+	public static int count(List<Integer> list, int amount) {
+		int n = list.size();
+		int[][] dp = new int[amount + 1][n];
+		// fill the entries for 0 value case
+		fill(dp[0], 1);
 
-    int n = a.length;
-    int[][] dp = new int[max + 1][n];
-    // fill the entries for 0 value case
-    fill(dp[0], 1);
+		for (int sum = 1; sum <= amount; ++sum) {
+			for (int i = 0; i < n; ++i) {
+				int excl = 0;
+				if (i > 0) {
+					excl = dp[sum][i - 1];
+				}
+				int incl = 0;
+				if (sum >= list.get(i)) {
+					incl = dp[sum - list.get(i)][i];
+				}
+				// total count
+				dp[sum][i] = incl + excl;
+			}
+		}
+		return dp[amount][n - 1];
+	}
 
-    for (int v = 1; v <= max; ++v) {
-      for (int i = 0; i < n; ++i) {
-        int excl = 0;
-        if (i > 0) {
-          excl = dp[v][i - 1];
-        }
-
-        int incl = 0;
-        if (v >= a[i]) {
-          incl = dp[v - a[i]][i];
-        }
-
-        // total count
-        dp[v][i] = incl + excl;
-      }
-    }
-    return dp[max][n - 1];
-  }
-
-  public static void main(String[] args) {
-    int[] coins = { 1, 2, 3 };
-    System.out.println(change(coins, 4));
-  }
+	public static void main(String[] args) {
+		System.out.println(count(asList(1, 2, 3), 4)); // 4
+		System.out.println(count(asList(1, 2, 5), 5)); // 4
+		System.out.println(count(asList(2), 3)); // 0
+	}
 }
--- src/main/java/com/svetanis/algorithms/dp/coinchange/CoinChangeSpaceOptimized.java
+++ src/main/java/com/svetanis/algorithms/dp/coinchange/CoinChangeSpaceOptimized.java
@@ -1,44 +1,44 @@
 package com.svetanis.algorithms.dp.coinchange;
 
+import static java.util.Arrays.asList;
+
+import java.util.List;
+
+// Given a value N, if we want to make change for N cents, 
+// and we have infinite supply of each of S = { S1, S2, .. , Sm} valued coins,
+// how many ways can we make the change? The order of coins doesn’t matter.
+
+// n - size of array of coins a
+// v - coin value
+// dp[i] will be storing the number
+// of solutions for value i.
+// we need n + 1 rows as the table is
+// constructed in bottom up manner
+// using the base case (n = 0)
+
 public final class CoinChangeSpaceOptimized {
 
-//Given a value N, if we want to make change for N cents, 
-//and we have infinite supply of each of S = { S1, S2, .. , Sm} valued coins,
-//how many ways can we make the change? The order of coins doesn’t matter.
+	public static int count(List<Integer> list, int amount) {
+		int[] dp = new int[amount + 1];
+		// base case
+		// given value is 0
+		dp[0] = 1;
+		// pick all coins one by one and
+		// update the dp[] values
+		// after the index greater than
+		// or equal to the value of
+		// the picked coin
+		for (int i = 0; i < list.size(); ++i) {
+			for (int sum = list.get(i); sum <= amount; sum++) {
+				dp[sum] = (dp[sum] + dp[sum - list.get(i)]) % 1000007;
+			}
+		}
+		return dp[amount] % 1000007;
+	}
 
-  // n - size of array of coins a
-  // v - coin value
-  // dp[i] will be storing the number
-  // of solutions for value i.
-  // we need n + 1 rows as the table is
-  // constructed in bottom up manner
-  // using the base case (n = 0)
-
-  public static int change(int[] a, int max) {
-
-    int n = a.length;
-    int[] dp = new int[max + 1];
-
-    // base case
-    // given value is 0
-    dp[0] = 1;
-
-    // pick all coins one by one and
-    // update the dp[] values
-    // after the index greater than
-    // or equal to the value of
-    // the picked coin
-    for (int i = 0; i < n; ++i) {
-      for (int v = a[i]; v <= max; v++) {
-        dp[v] = (dp[v] + dp[v - a[i]]) % 1000007;
-      }
-    }
-    return dp[max] % 1000007;
-  }
-
-  public static void main(String[] args) {
-    int v = 4;
-    int[] coins = { 1, 2, 3 };
-    System.out.println(change(coins, v));
-  }
+	public static void main(String[] args) {
+		System.out.println(count(asList(1, 2, 3), 4)); // 4
+		System.out.println(count(asList(1, 2, 5), 5)); // 4
+		System.out.println(count(asList(2), 3)); // 0
+	}
 }
