f5b062e6b4328a29ffa8971cc508415b7fb623b7
svetanis <svet125@gmail.com>
2024-11-02 23:21:38 -0700

    dp: coin change

--- src/main/java/com/svetanis/algorithms/dp/coinchange/CoinChangeRecursive.java
+++ src/main/java/com/svetanis/algorithms/dp/coinchange/CoinChangeRecursive.java
@@ -1,5 +1,9 @@
 package com.svetanis.algorithms.dp.coinchange;
 
+import static java.util.Arrays.asList;
+
+import java.util.List;
+
 // Given a value N, if we want to make change for N cents, 
 // and we have infinite supply of each of S = { S1, S2, .. , Sm} valued coins,
 // how many ways can we make the change? The order of coins doesn’t matter.
@@ -17,36 +21,36 @@ package com.svetanis.algorithms.dp.coinchange;
 
 public final class CoinChangeRecursive {
 
-	public static int count(int[] a, int v) {
-		int n = a.length;
-		return count(a, n, v);
+	public static int count(List<Integer> list, int amount) {
+		return count(list, 0, amount);
 	}
 
-	private static int count(int[] a, int n, int v) {
+	private static int count(List<Integer> list, int index, int amount) {
 		// base case
-		if (v == 0) {
+		if (amount == 0) {
 			return 1;
 		}
 		// if V < 0 then no solution exists
-		if (v < 0 || n < 0) {
+		if (amount < 0) {
 			return 0;
 		}
 		// if there are no coins and V > 0,
 		// then no solution exists
-		if (n <= 0 && v >= 1) {
+		if (index >= list.size() && amount >= 1) {
 			return 0;
 		}
 
 		// return the sum of solutions
 		// 1. include a[n - 1]: count(a[], n, v - a[n-1])
-		int incl = count(a, n, v - a[n - 1]);
+		int incl = count(list, index, amount - list.get(index));
 		// 2. excluding a[n - 1]: count(a[], n - 1, v)
-		int excl = count(a, n - 1, v);
+		int excl = count(list, index + 1, amount);
 		return incl + excl;
 	}
 
 	public static void main(String[] args) {
-		int[] coins = { 1, 2, 3 };
-		System.out.println(count(coins, 4));
+		System.out.println(count(asList(1, 2, 3), 4)); // 4
+		System.out.println(count(asList(1, 2, 5), 5)); // 4
+		System.out.println(count(asList(2), 3)); // 0
 	}
 }
--- src/main/java/com/svetanis/algorithms/dp/coinchange/CoinChangeTopDown.java
+++ src/main/java/com/svetanis/algorithms/dp/coinchange/CoinChangeTopDown.java
@@ -1,5 +1,9 @@
 package com.svetanis.algorithms.dp.coinchange;
 
+import static java.util.Arrays.asList;
+
+import java.util.List;
+
 // Given a value N, if we want to make change for N cents, 
 // and we have infinite supply of each of S = { S1, S2, .. , Sm} valued coins,
 // how many ways can we make the change? The order of coins doesn’t matter.
@@ -17,42 +21,43 @@ package com.svetanis.algorithms.dp.coinchange;
 
 public final class CoinChangeTopDown {
 
-  public static int count(int[] a, int v) {
-    int n = a.length;
-    Integer[][] dp = new Integer[a.length + 1][v + 1];
-    return count(a, n, v, dp);
-  }
+	public static int count(List<Integer> list, int amount) {
+		int n = list.size();
+		Integer[][] dp = new Integer[n + 1][amount + 1];
+		return count(list, 0, amount, dp);
+	}
 
-  private static int count(int[] a, int n, int v, Integer[][] dp) {
-    // base case
-    if (v == 0) {
-      return 1;
-    }
-    // if V < 0 then no solution exists
-    if (v < 0) {
-      return 0;
-    }
-    // if there are no coins and V > 0,
-    // then no solution exists
-    if (n <= 0 && v >= 1) {
-      return 0;
-    }
-    
-    if(dp[n][v] != null) {
-      return dp[n][v];
-    }
+	private static int count(List<Integer> list, int index, int amount, Integer[][] dp) {
+		// base case
+		if (amount == 0) {
+			return 1;
+		}
+		// if V < 0 then no solution exists
+		if (amount < 0) {
+			return 0;
+		}
+		// if there are no coins and V > 0,
+		// then no solution exists
+		if (index >= list.size() && amount >= 1) {
+			return 0;
+		}
 
-    // return the sum of solutions
-    // 1. include a[n - 1]: count(a[], n, v - a[n-1])
-    int incl = count(a, n, v - a[n - 1], dp);
-    // 2. excluding a[n - 1]: count(a[], n - 1, v)
-    int excl = count(a, n - 1, v, dp);
-    dp[n][v] = incl + excl;
-    return dp[n][v];
-  }
+		if (dp[index][amount] != null) {
+			return dp[index][amount];
+		}
 
-  public static void main(String[] args) {
-    int[] coins = { 1, 2, 3 };
-    System.out.println(count(coins, 4));
-  }
+		// return the sum of solutions
+		// 1. include a[n - 1]: count(a[], n, v - a[n-1])
+		int incl = count(list, index, amount - list.get(index), dp);
+		// 2. excluding a[n - 1]: count(a[], n - 1, v)
+		int excl = count(list, index + 1, amount, dp);
+		dp[index][amount] = incl + excl;
+		return dp[index][amount];
+	}
+
+	public static void main(String[] args) {
+		System.out.println(count(asList(1, 2, 3), 4)); // 4
+		System.out.println(count(asList(1, 2, 5), 5)); // 4
+		System.out.println(count(asList(2), 3)); // 0
+	}
 }
