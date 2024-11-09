93291473d36c4d5d300a077639c2ec2b21fb1046
svetanis <svet125@gmail.com>
2024-11-08 20:17:04 -0700

    combination sum

---
+++ src/main/java/com/svetanis/algorithms/dp/coinchange/CombinationSumBottomUp.java
@@ -0,0 +1,35 @@
+package com.svetanis.algorithms.dp.coinchange;
+
+// 377. Combination Sum IV
+
+// given an array of distinct integers and
+// a target integer, return the number of 
+// possible combinations that add up to target
+
+public final class CombinationSumBottomUp {
+	// Time Complexity: O(target * n)
+	// Space Complexity: O(target)
+
+	public static int count(int[] nums, int target) {
+		int[] dp = new int[target + 1];
+		dp[0] = 1;
+		for (int sum = 1; sum <= target; ++sum) {
+			for (int num : nums) {
+				if (sum >= num) {
+					dp[sum] += dp[sum - num];
+				}
+			}
+		}
+		return dp[target];
+	}
+
+	public static void main(String[] args) {
+		int[] a1 = { 1, 2, 3 };
+		int[] a2 = { 9 };
+		int[] a3 = { 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25 };
+		System.out.println(count(a1, 4)); // 7
+		System.out.println(count(a2, 3)); // 0
+		System.out.println(count(a3, 10)); // 9
+
+	}
+}
---
+++ src/main/java/com/svetanis/algorithms/dp/coinchange/CombinationSumTopDown.java
@@ -0,0 +1,44 @@
+package com.svetanis.algorithms.dp.coinchange;
+
+import java.util.HashMap;
+import java.util.Map;
+
+// 377. Combination Sum IV
+
+// given an array of distinct integers and
+// a target integer, return the number of 
+// possible combinations that add up to target
+
+public final class CombinationSumTopDown {
+
+	public static int count(int[] nums, int target) {
+		Map<Integer, Integer> map = new HashMap<>();
+		return combinations(nums, target, map);
+	}
+
+	private static int combinations(int[] nums, int remain, Map<Integer, Integer> map) {
+		if (remain == 0) {
+			return 1;
+		}
+		if (map.containsKey(remain)) {
+			return map.get(remain);
+		}
+		int sum = 0;
+		for (int num : nums) {
+			if (remain - num >= 0) {
+				sum += combinations(nums, remain - num, map);
+			}
+		}
+		map.put(remain, sum);
+		return sum;
+	}
+
+	public static void main(String[] args) {
+		int[] a1 = { 1, 2, 3 };
+		int[] a2 = { 9 };
+		int[] a3 = { 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25 };
+		System.out.println(count(a1, 4)); // 7
+		System.out.println(count(a2, 3)); // 0
+		System.out.println(count(a3, 10)); // 9
+	}
+}
