41dc28d93f821679a7df73ff752935b7b6c10ddb
svetanis <svet125@gmail.com>
2024-11-13 17:49:51 -0700

    count palindromic substr

--- src/main/java/com/svetanis/algorithms/dp/palindrome/CountPalindromeSubStrBottomUp.java
+++ src/main/java/com/svetanis/algorithms/dp/palindrome/CountPalindromeSubStrBottomUp.java
@@ -1,26 +1,24 @@
 package com.svetanis.algorithms.dp.palindrome;
 
-// Given a string, find the total number of palindromic substrings in it. 
+// 647. Palindromic Substrings
+
+// Given a string, find the total number 
+// of palindromic substrings in it. 
 
 public final class CountPalindromeSubStrBottomUp {
+  // Time Complexity: O(n^2)
+  // Auxiliary Space O(n^2)
 
   public static int lps(String str) {
-    // Time Complexity: O(n^2)
-    // Auxiliary Space O(n^2)
-
     int n = str.length();
     int count = 0;
-
-    // table to store results of subproblems
     boolean[][] dp = new boolean[n][n];
-
     // strings of length 1 are
     // palindrome of length 1
     for (int i = 0; i < n; ++i) {
       dp[i][i] = true;
       count++;
     }
-
     // check for substring of length 2
     for (int i = 0; i < n - 1; ++i) {
       if (str.charAt(i) == str.charAt(i + 1)) {
