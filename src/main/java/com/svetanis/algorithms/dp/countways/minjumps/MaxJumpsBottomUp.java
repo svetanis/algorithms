c4a7d18c707297bb7954f8a6d283f58a249f74f3
svetanis <svet125@gmail.com>
2024-11-17 15:46:53 -0700

    max jumps to reach end of array

---
+++ src/main/java/com/svetanis/algorithms/dp/countways/minjumps/MaxJumpsBottomUp.java
@@ -0,0 +1,42 @@
+package com.svetanis.algorithms.dp.countways.minjumps;
+
+import java.util.Arrays;
+
+// 2770. Maximum Number of Jumps to Reach the Last Index
+
+public final class MaxJumpsBottomUp {
+	// Time Complexity: O(n^2)
+	// Space Complexity: O(n)
+
+	private final static int INF = -Integer.MAX_VALUE;
+
+	public static int count(int[] a, int target) {
+		int n = a.length;
+		int[] dp = new int[n];
+		Arrays.fill(dp, INF);
+		dp[0] = 0;
+		for (int start = 0; start < n - 1; start++) {
+			for (int end = start + 1; end < n; end++) {
+				int jump = Math.abs(a[end] - a[start]);
+				if (jump <= target && dp[start] != INF) {
+					dp[end] = Math.max(dp[end], dp[start] + 1);
+				}
+			}
+		}
+		return dp[n - 1] == INF ? -1 : dp[n - 1];
+	}
+
+	public static void main(String[] args) {
+		int[] a1 = { 1, 3, 6, 4, 1, 2 };
+		System.out.println(count(a1, 2)); // 3
+
+		int[] a2 = { 1, 3, 6, 4, 1, 2 };
+		System.out.println(count(a2, 3)); // 5
+
+		int[] a3 = { 1, 3, 6, 4, 1, 2 };
+		System.out.println(count(a3, 0)); // -1
+
+		int[] a4 = { 0, 2, 1, 3 };
+		System.out.println(count(a4, 1)); // -1
+	}
+}
---
+++ src/main/java/com/svetanis/algorithms/dp/countways/minjumps/MaxJumpsTopDown.java
@@ -0,0 +1,48 @@
+package com.svetanis.algorithms.dp.countways.minjumps;
+
+// 2770. Maximum Number of Jumps to Reach the Last Index
+
+public final class MaxJumpsTopDown {
+	// Time Complexity: O(n^2)
+	// Space Complexity: O(n)
+
+	private final static int INF = -Integer.MAX_VALUE;
+
+	public static int count(int[] a, int target) {
+		int n = a.length;
+		Integer[] dp = new Integer[n];
+		int max = count(a, target, 0, dp);
+		return max < 0 ? -1 : max;
+	}
+
+	private static int count(int[] a, int target, int index, Integer[] dp) {
+		if (index == a.length - 1) {
+			return 0;
+		}
+		if (dp[index] != null) {
+			return dp[index];
+		}
+		int max = INF;
+		for (int i = index + 1; i < a.length; i++) {
+			int jumps = Math.abs(a[index] - a[i]);
+			if (jumps <= target) {
+				max = Math.max(max, 1 + count(a, target, i, dp));
+			}
+		}
+		return dp[index] = max;
+	}
+
+	public static void main(String[] args) {
+		int[] a1 = { 1, 3, 6, 4, 1, 2 };
+		System.out.println(count(a1, 2)); // 3
+
+		int[] a2 = { 1, 3, 6, 4, 1, 2 };
+		System.out.println(count(a2, 3)); // 5
+
+		int[] a3 = { 1, 3, 6, 4, 1, 2 };
+		System.out.println(count(a3, 0)); // -1
+
+		int[] a4 = { 0, 2, 1, 3 };
+		System.out.println(count(a4, 1)); // -1
+	}
+}
