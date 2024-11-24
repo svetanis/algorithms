58474825d3cbac0488636878c65cd1e8c8b7676e
svetanis <svet125@gmail.com>
2024-11-24 13:25:39 -0700

    fibonacci

--- src/main/java/com/svetanis/algorithms/dp/math/fib/FibonacciBottomUp.java
+++ src/main/java/com/svetanis/algorithms/dp/math/fib/FibonacciBottomUp.java
@@ -5,15 +5,26 @@ package com.svetanis.algorithms.dp.math.fib;
 // F(0) = 0, F(1) = 1
 // F(n) = F(n - 1) + F(n - 2), for n > 1.
 
-public final class FibonacciRecursive {
+public final class FibonacciBottomUp {
+	// Time Complexity: O(n)
+	// Space Complexity: O(n)
 
 	public static int fib(int n) {
 		if (n == 0) {
 			return 0;
-		} else if (n == 1 || n == 2) {
+		}
+		if (n == 1 || n == 2) {
 			return 1;
 		}
-		return fib(n - 1) + fib(n - 2);
+
+		int[] dp = new int[n + 1];
+		dp[0] = 0;
+		dp[1] = 1;
+		dp[2] = 1;
+		for (int i = 3; i <= n; i++) {
+			dp[i] = dp[i - 1] + dp[i - 2];
+		}
+		return dp[n];
 	}
 
 	public static void main(String[] args) {
--- src/main/java/com/svetanis/algorithms/dp/math/fib/FibonacciSpaceOptimized.java
+++ src/main/java/com/svetanis/algorithms/dp/math/fib/FibonacciSpaceOptimized.java
@@ -5,15 +5,27 @@ package com.svetanis.algorithms.dp.math.fib;
 // F(0) = 0, F(1) = 1
 // F(n) = F(n - 1) + F(n - 2), for n > 1.
 
-public final class FibonacciRecursive {
-
+public final class FibonacciSpaceOptimized {
+	// Time Complexity: O(n)
+	// Space Complexity: O(1)
+	
 	public static int fib(int n) {
 		if (n == 0) {
 			return 0;
-		} else if (n == 1 || n == 2) {
+		}
+		if (n == 1 || n == 2) {
 			return 1;
 		}
-		return fib(n - 1) + fib(n - 2);
+
+		int fib1 = 1;
+		int fib2 = 1;
+		int fib = 1;
+		for (int i = 3; i <= n; i++) {
+			fib = fib1 + fib2;
+			fib1 = fib2;
+			fib2 = fib;
+		}
+		return fib;
 	}
 
 	public static void main(String[] args) {
