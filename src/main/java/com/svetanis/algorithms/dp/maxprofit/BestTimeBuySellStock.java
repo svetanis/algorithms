67712fa0169fa11740117cf89502cb8fc30e0aa9
svetanis <svet125@gmail.com>
2024-11-26 14:00:35 -0700

    best time buy and sell stocks

--- src/main/java/com/svetanis/algorithms/dp/maxprofit/BestTimeBuySellStock.java
+++ src/main/java/com/svetanis/algorithms/dp/maxprofit/BestTimeBuySellStock.java
@@ -3,13 +3,15 @@ package com.svetanis.algorithms.dp.maxprofit;
 import static java.lang.Math.max;
 import static java.lang.Math.min;
 
+// 121. Best Time to Buy and Sell Stock
+
 // maximize your profit by choosing a single day 
 // to buy one stock and choosing a different day 
 // in the future to sell that stock.
 
 public final class BestTimeBuySellStock {
 	// Time Complexity: O(n)
-	
+
 	public static int maxProfit(int[] prices) {
 		int max = 0; // max profit
 		int min = prices[0]; // min buying price
@@ -26,8 +28,8 @@ public final class BestTimeBuySellStock {
 
 	public static void main(String[] args) {
 		int[] a1 = { 7, 1, 5, 3, 6, 4 }; // 5
-		int[] a2 = { 7, 6, 4, 3, 1 };
 		System.out.println(maxProfit(a1));
+		int[] a2 = { 7, 6, 4, 3, 1 }; // 0
 		System.out.println(maxProfit(a2));
 	}
 }
