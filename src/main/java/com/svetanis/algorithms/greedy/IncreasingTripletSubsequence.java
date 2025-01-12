669b404647b6057fb75dc5dd35f632670382e425
svetanis <svet125@gmail.com>
2025-01-11 19:47:35 -0700

    increasing triplet subseq

--- src/main/java/com/svetanis/algorithms/greedy/IncreasingTripletSubsequence.java
+++ src/main/java/com/svetanis/algorithms/greedy/IncreasingTripletSubsequence.java
@@ -10,6 +10,21 @@ import java.util.List;
 public final class IncreasingTripletSubsequence {
 	// Time Complexity: O(n)
 
+	public static boolean increasingTriplet(int[] a) {
+		int low = Integer.MAX_VALUE;
+		int mid = Integer.MAX_VALUE;
+		for (int curr : a) {
+			if (curr <= low) {
+				low = curr;
+			} else if (curr <= mid) {
+				mid = curr;
+			} else { // curr > mid
+				return true;
+			}
+		}
+		return false;
+	}
+
 	public static boolean increasingTriplet(List<Integer> list) {
 		int low = MAX_VALUE;
 		int mid = MAX_VALUE;
@@ -30,5 +45,12 @@ public final class IncreasingTripletSubsequence {
 		System.out.println(increasingTriplet(asList(1, 2, 3, 4, 5))); // true
 		System.out.println(increasingTriplet(asList(5, 4, 3, 2, 1))); // false
 		System.out.println(increasingTriplet(asList(2, 1, 5, 0, 4, 6))); // true
+
+		int[] a1 = { 1, 2, 3, 4, 5 };
+		int[] a2 = { 5, 4, 3, 2, 1 };
+		int[] a3 = { 2, 1, 5, 0, 4, 6 };
+		System.out.println(increasingTriplet(a1)); // true
+		System.out.println(increasingTriplet(a2)); // false
+		System.out.println(increasingTriplet(a3)); // true
 	}
 }
