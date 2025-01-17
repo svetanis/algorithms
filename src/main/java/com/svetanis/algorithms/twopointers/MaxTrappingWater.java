0695e3c96b8c6259c21d3ebd1c5947ba45c1b751
svetanis <svet125@gmail.com>
2025-01-16 20:01:35 -0700

    max trapping water

--- src/main/java/com/svetanis/algorithms/twopointers/MaxTrappingWater.java
+++ src/main/java/com/svetanis/algorithms/twopointers/MaxTrappingWater.java
@@ -6,6 +6,8 @@ import static java.util.Arrays.asList;
 
 import java.util.List;
 
+// 11. Container With Most Water
+
 // Suppose you are given an array containing non-negative numbers 
 // representing heights of a set of buildings. 
 // Now, because of differences in heights of buildings water can be trapped between them. 
@@ -33,10 +35,36 @@ public final class MaxTrappingWater {
 		return max;
 	}
 
+	public static int maxArea(int[] a) {
+		int left = 0;
+		int right = a.length - 1;
+		int max = Integer.MIN_VALUE;
+		while (left < right) {
+			int min = Math.min(a[left], a[right]);
+			int area = min * (right - left);
+			max = Math.max(max, area);
+			if (a[left] < a[right]) {
+				++left;
+			} else {
+				--right;
+			}
+		}
+		return max;
+	}
+
 	public static void main(String[] args) {
 		System.out.println(maxWater(asList(2, 0, 2)));
 		System.out.println(maxWater(asList(3, 0, 0, 2, 0, 4)));
 		System.out.println(maxWater(asList(0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1)));
 		System.out.println(maxWater(asList(1, 8, 6, 2, 5, 4, 8, 3, 7)));
+
+		int[] a = { 2, 5, 8, 1, 3, 7 };
+		System.out.println(maxArea(a)); // 21
+
+		int[] a1 = { 1, 8, 6, 2, 5, 4, 8, 3, 7 };
+		System.out.println(maxArea(a1)); // 49
+
+		int[] a2 = { 1, 1 };
+		System.out.println(maxArea(a2)); // 1
 	}
 }
