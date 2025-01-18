ae380176f37ebfc1f7c633568da292b0eb1c3773
svetanis <svet125@gmail.com>
2025-01-18 13:51:23 -0700

    max sum subarray kadane

--- src/main/java/com/svetanis/algorithms/dp/sum/max/subarr/MaxSumSubArrayKadaneAllNeg.java
+++ src/main/java/com/svetanis/algorithms/dp/sum/max/subarr/MaxSumSubArrayKadaneAllNeg.java
@@ -1,35 +1,35 @@
 package com.svetanis.algorithms.dp.sum.max.subarr;
 
-import static java.lang.Math.max;
+// 53. Maximum subarray
 
 public final class MaxSumSubArrayKadaneAllNeg {
+	// Time Complexity: O(n)
+	// Space Complexity: O(1)
 
-  public static int kadane(int[] a) {
-    int n = a.length;
-    int max = a[0];
-    int sum = a[0];
+	public static int kadane(int[] a) {
+		int max = a[0];
+		int sum = a[0];
+		for (int i = 1; i < a.length; ++i) {
+			sum = Math.max(sum, 0) + a[i];
+			max = Math.max(max, sum);
+		}
+		return max;
+	}
 
-    for (int i = 1; i < n; ++i) {
-      sum = max(a[i], sum + a[i]);
-      max = max(max, sum);
-    }
-    return max;
-  }
+	public static void main(String[] args) {
+		int[] a1 = { -2, -3, -4, -1, -2, -1, -5, -3 };
+		System.out.println(kadane(a1));
 
-  public static void main(String[] args) {
-    int[] a1 = { -2, -3, -4, -1, -2, -1, -5, -3 };
-    System.out.println(kadane(a1));
+		int[] a2 = { -1, 4, -2, 5, -5, 2, -20, 6 };
+		System.out.println(kadane(a2));
 
-    int[] a2 = { -1, 4, -2, 5, -5, 2, -20, 6 };
-    System.out.println(kadane(a2));
+		int[] a3 = { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
+		System.out.println(kadane(a3));
 
-    int[] a3 = { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
-    System.out.println(kadane(a3));
-    
-    int[] a4 = {2, -8, 3, -2, 4, -10};
-    System.out.println(kadane(a4));
+		int[] a4 = { 2, -8, 3, -2, 4, -10 };
+		System.out.println(kadane(a4));
 
-    int[] a5 = {-3, -10, -5};
-    System.out.println(kadane(a5));
-  }
+		int[] a5 = { -3, -10, -5 };
+		System.out.println(kadane(a5));
+	}
 }
