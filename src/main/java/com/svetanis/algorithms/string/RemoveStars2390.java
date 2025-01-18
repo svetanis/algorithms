d9986f9a55eb724a66023aef45cebe9032374775
svetanis <svet125@gmail.com>
2025-01-18 16:21:32 -0700

    remove stars

---
+++ src/main/java/com/svetanis/algorithms/string/RemoveStars2390.java
@@ -0,0 +1,27 @@
+package com.svetanis.algorithms.string;
+
+// 2390. Removing Stars From a String
+
+public final class RemoveStars2390 {
+  // Time Complexity: O(n)
+  // Space Complexity: O(n)
+
+  public static String remove(String s) {
+    StringBuilder sb = new StringBuilder();
+    for (char c : s.toCharArray()) {
+      if (c == '*') {
+        if (sb.length() > 0) {
+          sb.deleteCharAt(sb.length() - 1);
+        }
+      } else {
+        sb.append(c);
+      }
+    }
+    return sb.toString();
+  }
+
+  public static void main(String[] args) {
+    System.out.println(remove("leet**cod*e")); // lecoe
+    System.out.println(remove("erase*****")); // ""
+  }
+}
