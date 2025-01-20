cbfcf1b01b81a9a0d35d716805456c6072d768f5
svetanis <svet125@gmail.com>
2025-01-19 19:30:45 -0700

    binary: search insert position

--- src/main/java/com/svetanis/algorithms/search/binary/BinarySearchInsertPositionIterative.java
+++ src/main/java/com/svetanis/algorithms/search/binary/BinarySearchInsertPositionIterative.java
@@ -1,36 +1,50 @@
 package com.svetanis.algorithms.search.binary;
 
+// 35. Search Insert Position
+
 // given a sorted array and a target value
 // find the index if the target is found
 // if not, return the index where it would
 // be if it were inserted in order
 
 public final class BinarySearchInsertPositionIterative {
+	// Time Complexity: O(log n)
 
-  public static int binary(int[] a, int x) {
-    // O(log n)
+	public static int binary(int[] a, int target) {
+		int left = 0;
+		int right = a.length - 1;
+		while (left <= right) {
+			int mid = left + (right - left) / 2;
+			if (target > a[mid]) {
+				left = mid + 1;
+			} else if (target < a[mid]) {
+				right = mid - 1;
+			} else {
+				return mid;
+			}
+		}
+		return left;
+	}
 
-  	int left = 0;
-    int right = a.length - 1;
+	public static int binary2(int[] a, int target) {
+		int left = 0;
+		int right = a.length;
+		while (left < right) {
+			int mid = left + (right - left) / 2;
+			if (a[mid] >= target) {
+				right = mid;
+			} else {
+				left = mid + 1;
+			}
+		}
+		return left;
+	}
 
-    while (left <= right) {
-      int mid = left + (right - left) / 2;
-      if (x > a[mid]) {
-        left = mid + 1;
-      } else if (x < a[mid]) {
-        right = mid - 1;
-      } else {
-        return mid;
-      }
-    }
-    return left;
-  }
-
-  public static void main(String[] args) {
-    int[] a = { 1, 3, 5, 6 };
-    System.out.println(binary(a, 5)); // 2
-    System.out.println(binary(a, 2)); // 1
-    System.out.println(binary(a, 7)); // 4
-    System.out.println(binary(a, 0)); // 0
-  }
+	public static void main(String[] args) {
+		int[] a = { 1, 3, 5, 6 };
+		System.out.println(binary(a, 5)); // 2
+		System.out.println(binary(a, 2)); // 1
+		System.out.println(binary(a, 7)); // 4
+		System.out.println(binary(a, 0)); // 0
+	}
 }
--- src/main/java/com/svetanis/algorithms/search/binary/BinarySearchInsertPositionRecursive.java
+++ src/main/java/com/svetanis/algorithms/search/binary/BinarySearchInsertPositionRecursive.java
@@ -1,26 +1,28 @@
 package com.svetanis.algorithms.search.binary;
 
+// 35. Search Insert Position
+
 // given a sorted array and a target value
 // find the index if the target is found
 // if not, return the index where it would
 // be if it were inserted in order
 
 public final class BinarySearchInsertPositionRecursive {
+	// Time Complexity: O(log n)
 
-	public static int binary(int[] a, int x) {
-		// O(log n)
-		return binary(a, 0, a.length - 1, x);
+	public static int binary(int[] a, int target) {
+		return binary(a, 0, a.length - 1, target);
 	}
 
-	private static int binary(int[] a, int left, int right, int x) {
+	private static int binary(int[] a, int left, int right, int target) {
 		if (left > right) {
 			return left;
 		}
 		int mid = left + (right - left) / 2;
-		if (x < a[mid]) {
-			return binary(a, left, mid - 1, x);
-		} else if (x > a[mid]) {
-			return binary(a, mid + 1, right, x);
+		if (target < a[mid]) {
+			return binary(a, left, mid - 1, target);
+		} else if (target > a[mid]) {
+			return binary(a, mid + 1, right, target);
 		} else {
 			return mid;
 		}
