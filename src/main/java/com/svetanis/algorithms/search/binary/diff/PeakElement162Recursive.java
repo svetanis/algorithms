80f26b256699288a883eabb41438d6f31584ed0f
svetanis <svet125@gmail.com>
2025-01-19 20:01:30 -0700

    binary search: peak element

---
+++ src/main/java/com/svetanis/algorithms/search/binary/diff/PeakElement162Recursive.java
@@ -0,0 +1,56 @@
+package com.svetanis.algorithms.search.binary.diff;
+
+// 162. Find Peak Element
+
+// Given an array of integers. Find a peak element in it. 
+// An array element is peak if it is greater than its neighbors. 
+
+// if mid element is not peak and 
+// its left neighbor is greater than it
+// then left half must have a peak element
+
+// if mid element is not peak and 
+// its right neighbor is greater than it
+// then right half must have a peak element
+
+public final class PeakElement162Recursive {
+	// Time Complexity: O(log n)
+
+	private static int peak(int[] a) {
+		return peak(a, 0, a.length - 1);
+	}
+
+	private static int peak(int[] a, int left, int right) {
+		if (right < left) {
+			return -1;
+		}
+		int n = a.length;
+		int mid = left + (right - left) / 2;
+		boolean one = mid == 0 || a[mid - 1] < a[mid];
+		boolean two = mid == n - 1 || a[mid + 1] < a[mid];
+		if (one && two) {
+			return mid;
+		} else if (mid > 0 && a[mid - 1] > a[mid]) {
+			return peak(a, left, mid - 1);
+		} else {
+			return peak(a, mid + 1, right);
+		}
+	}
+
+	public static void main(String[] args) {
+		int[] a1 = { 1, 3, 20, 4, 1, 0 };
+		System.out.println(peak(a1)); // 2
+
+		int[] a2 = { 10, 20, 15, 2, 23, 90, 67 };
+		System.out.println(peak(a2)); // 1
+
+		int[] a3 = { 10, 20, 30, 40, 50 };
+		System.out.println(peak(a3)); // 4
+
+		int[] a4 = { 100, 80, 60, 50, 20 };
+		System.out.println(peak(a4)); // 0
+
+		int[] a5 = { 20, 20, 20, 20, 20 };
+		System.out.println(peak(a5)); // -1
+	}
+}
--- src/main/java/com/svetanis/algorithms/search/binary/diff/PeakElementRecursive.java
+++ src/main/java/com/svetanis/algorithms/search/binary/diff/PeakElementRecursive.java
@@ -1,7 +1,10 @@
 package com.svetanis.algorithms.search.binary.diff;
 
-// Given an array of integers. Find a peak element in it. 
-// An array element is peak if it is greater than its neighbors. 
+// given an array of integers
+// find a peak element in it
+
+// an array element is peak if 
+// it is NOT smaller than its neighbors
 
 // if mid element is not peak and 
 // its left neighbor is greater than it
@@ -11,24 +14,32 @@ package com.svetanis.algorithms.search.binary.diff;
 // its right neighbor is greater than it
 // then right half must have a peak element
 
-public final class PeakElementModified {
+// corner cases:
+// if array is sorted in strictly increasing order,
+// the last element is always a peak element
 
-  private static int peak(int[] a) {
-    return peak(a, 0, a.length - 1);
+// if array is sorted in strictly decreasing order,
+// the first element is always a peak element
+
+// if all elements of array are same,
+// every element is a peak element
+
+public final class PeakElementRecursive {
+  // Time Complexity: O(log n)
+
+  public static int peak(int[] a) {
+    int n = a.length;
+    return peak(a, 0, n - 1);
   }
 
   private static int peak(int[] a, int left, int right) {
-    // Time Complexity: O(log n)
-
     if (right < left) {
       return -1;
     }
-
-    int mid = left + (right - left) / 2;
     int n = a.length;
-
-    boolean one = mid == 0 || a[mid - 1] < a[mid];
-    boolean two = mid == n - 1 || a[mid + 1] < a[mid];
+    int mid = left + (right - left) / 2;
+    boolean one = mid == 0 || a[mid - 1] <= a[mid];
+    boolean two = mid == n - 1 || a[mid + 1] <= a[mid];
     if (one && two) {
       return mid;
     } else if (mid > 0 && a[mid - 1] > a[mid]) {
@@ -51,7 +62,7 @@ public final class PeakElementModified {
     int[] a4 = { 100, 80, 60, 50, 20 };
     System.out.println(peak(a4)); // 0
 
-    int[] a5 = { 20, 20, 20, 20, 20 }; 
-    System.out.println(peak(a5)); // -1
+    int[] a5 = { 20, 20, 20, 20, 20 };
+    System.out.println(peak(a5)); // 2
   }
 }
