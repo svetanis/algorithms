242dc2e88d1d475b085f3736eb22ae8a2245cec9
svetanis <svet125@gmail.com>
2025-01-23 20:06:15 -0700

    string anagrams

--- src/main/java/com/svetanis/algorithms/slidingwindow/string/anagram/StringAnagramsSubmit.java
+++ src/main/java/com/svetanis/algorithms/slidingwindow/string/anagram/StringAnagramsSubmit.java
@@ -5,9 +5,8 @@ import static com.svetanis.java.base.collect.Lists.newList;
 import static com.svetanis.java.base.utils.Print.print;
 
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 
 import com.google.common.collect.ImmutableList;
 import com.svetanis.java.base.Pair;
@@ -17,45 +16,34 @@ import com.svetanis.java.base.Pair;
 // Given a string and a pattern, find all anagrams 
 // of the pattern in the given string.
 
-public final class StringAnagrams {
+public final class StringAnagramsSubmit {
 	// Time complexity: O(n)
 
 	public static List<Integer> anagrams(String s, String p) {
 		int n = s.length();
-		int left = 0;
-		int matched = 0;
+		int m = p.length();
+		if (n < m) {
+			return new ArrayList<>();
+		}
+		int[] pcount = frequencies(p, m);
+		int[] wcount = frequencies(s, m - 1);
 		List<Integer> list = new ArrayList<>();
-		Map<Character, Integer> map = frequencies(p);
-		for (int right = 0; right < n; right++) {
-			char end = s.charAt(right);
-			if (map.containsKey(end)) {
-				map.put(end, map.get(end) - 1);
-				if (map.get(end) == 0) {
-					matched++;
-				}
+		for (int i = m - 1; i < n; i++) {
+			wcount[s.charAt(i) - 'a']++;
+			if (Arrays.equals(pcount, wcount)) {
+				list.add(i - m + 1);
 			}
-			if (matched == map.size()) {
-				list.add(left);
-			}
-			if (right >= p.length() - 1) {
-				char start = s.charAt(left++);
-				if (map.containsKey(start)) {
-					if (map.get(start) == 0) {
-						matched--;
-					}
-					map.put(start, map.get(start) + 1);
-				}
-			}
+			wcount[s.charAt(i - m + 1) - 'a']--;
 		}
 		return list;
 	}
 
-	private static Map<Character, Integer> frequencies(String s) {
-		Map<Character, Integer> map = new HashMap<>();
-		for (char c : s.toCharArray()) {
-			map.put(c, map.getOrDefault(c, 0) + 1);
+	private static int[] frequencies(String s, int size) {
+		int[] a = new int[26];
+		for (int i = 0; i < size; i++) {
+			a[s.charAt(i) - 'a']++;
 		}
-		return map;
+		return a;
 	}
 
 	public static void main(String[] args) {
@@ -71,8 +59,6 @@ public final class StringAnagrams {
 		list.add(Pair.build("abbcabc", "abc")); // 2, 3, 4
 		list.add(Pair.build("cbaebabacd", "abc")); // 0, 6
 		list.add(Pair.build("abab", "ab")); // 0, 1, 2
-		list.add(Pair.build("BACDGABCDA", "ABCD")); // 0, 5, 6
-		list.add(Pair.build("AAABABAA", "AABA")); // 0, 1, 4
 		return newList(list);
 	}
 
