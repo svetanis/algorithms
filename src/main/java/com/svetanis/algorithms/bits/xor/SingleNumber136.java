449aee0486a02780e144ccce456f26cb062c3e4c
svetanis <svet125@gmail.com>
2025-01-25 13:21:05 -0700

    single number

---
+++ src/main/java/com/svetanis/algorithms/bits/xor/SingleNumber136.java
@@ -0,0 +1,33 @@
+package com.svetanis.algorithms.bits.xor;
+
+// 136. Single Number
+
+// in a non-empty array of integers,
+// every number appears twice except
+// for one, find that single number
+
+// XOR of two same numbers returns zero
+// XOR with zero returns the same number
+
+public final class SingleNumber136 {
+  // Time Complexity: O(n)
+
+  public static int single(int[] a) {
+    int xor = 0;
+    for (int num : a) {
+      xor = xor ^ num;
+    }
+    return xor;
+  }
+
+  public static void main(String[] args) {
+    int[] a1 = { 2, 2, 1 };
+    System.out.println(single(a1)); // 1
+
+    int[] a2 = { 4, 1, 2, 1, 2 };
+    System.out.println(single(a2)); // 4
+
+    int[] a3 = { 1 };
+    System.out.println(single(a3)); // 1
+  }
+}
