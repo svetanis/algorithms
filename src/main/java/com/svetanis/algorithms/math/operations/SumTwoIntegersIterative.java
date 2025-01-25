a9b81fbf42008990dbaa0bd37908526faca87751
svetanis <svet125@gmail.com>
2025-01-25 13:07:50 -0700

    sum two integers

--- src/main/java/com/svetanis/algorithms/math/operations/SumTwoIntegersIterative.java
+++ src/main/java/com/svetanis/algorithms/math/operations/SumTwoIntegersIterative.java
@@ -10,9 +10,16 @@ public final class SumTwoIntegersIterative {
 	// Time Complexity: O(n)
 
 	public static int sum(int x, int y) {
+		// iterate till there is no carry
 		while (y != 0) {
+			// carry now contains
+			// common set bits of x and y
 			int carry = x & y;
+			// sum of bits of x and y where
+			// at least one of the bits is not set
 			x = x ^ y;
+			// carry is shifted by one so that
+			// adding it to x gives the required sum
 			y = carry << 1;
 		}
 		return x;
