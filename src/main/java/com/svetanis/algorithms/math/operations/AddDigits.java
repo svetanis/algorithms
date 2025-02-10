c34bb048adcac5b6478f54ba5f1d96decccb1946
svetanis <svet125@gmail.com>
2025-02-09 21:55:23 -0700

    add digits

---
+++ src/main/java/com/svetanis/algorithms/math/operations/AddDigits.java
@@ -0,0 +1,30 @@
+package com.svetanis.algorithms.math.operations;
+
+// 258. Add Digits
+
+public final class AddDigits {
+  // Time Complexity: O(n)
+  // Space Complexity: O(n)
+
+  public static int addDigits(int n) {
+  	int sum = 0;
+  	while(n > 0) {
+  		sum += n % 10;
+  		n = n/10;
+  		if(n == 0 && sum > 9) {
+  			n = sum;
+  			sum = 0;
+  		}
+  	}
+  	return sum;
+  }
+  
+  public static int addDigits2(int n) {
+  	return (n - 1) % 9 + 1;
+  }
+
+  public static void main(String[] args) {
+    System.out.println(addDigits(38)); // 2
+    System.out.println(addDigits(0)); // 0
+  }
+}
