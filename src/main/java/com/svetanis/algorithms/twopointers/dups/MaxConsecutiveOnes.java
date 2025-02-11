aabf919628de9c22816ff11a8dd415b75d2609c8
svetanis <svet125@gmail.com>
2025-02-10 21:14:32 -0700

    max consecutive ones

---
+++ src/main/java/com/svetanis/algorithms/twopointers/dups/MaxConsecutiveOnes.java
@@ -0,0 +1,30 @@
+package com.svetanis.algorithms.twopointers.dups;
+
+// 485. Max Consecutive Ones
+
+public final class MaxConsecutiveOnes {
+	// Time Complexity: O(n)
+	// Aux Space: O(1)
+
+	public static int count(int[] a) {
+		int count = 0;
+		int max = 0;
+		for (int i = 0; i < a.length; i++) {
+			if (a[i] == 1) {
+				count++;
+			} else {
+				max = Math.max(max, count);
+				count = 0;
+			}
+		}
+		max = Math.max(max, count);		
+		return max;
+	}
+
+	public static void main(String[] args) {
+		int[] a = { 1, 1, 0, 1, 1, 1 };
+		System.out.println(count(a)); // 3
+		int[] a1 = { 1, 0, 1, 1, 0, 1 };
+		System.out.println(count(a1)); // 2
+	}
+}
