4a121408937d14e860e11e992d0196b4076726c5
svetanis <svet125@gmail.com>
2025-02-15 10:42:02 -0700

    count subarrays product less than k

--- src/main/java/com/svetanis/algorithms/slidingwindow/array/CountSubArrsProductLessThanK.java
+++ src/main/java/com/svetanis/algorithms/slidingwindow/array/CountSubArrsProductLessThanK.java
@@ -1,47 +1,52 @@
 package com.svetanis.algorithms.slidingwindow.array;
 
+// 713. Subarray Product Less Than K
+
 // given array of positive integers and number K
 // count subarrays with product less than K
 
 public final class CountSubArrsProductLessThanK {
+	// Time Complexity: O(n)
+	// Space Complexity: O(1)
 
-  public static int count(int[] a, int k) {
-    // Time Complexity: O(n)
-    // Space Complexity: O(1)
-    int n = a.length;
-    int prod = 1;
-    int count = 0;
-    int left = 0;
+	public static int count(int[] a, int k) {
+		int n = a.length;
+		int prod = 1;
+		int count = 0;
+		int left = 0;
+		for (int right = 0; right < n; right++) {
+			prod *= a[right];
+			while (left < right && prod >= k) {
+				prod /= a[left++];
+			}
+			if (prod < k) {
+				count += (right - left + 1);
+			}
+		}
+		return count;
+	}
 
-    for (int right = 0; right < n; right++) {
-      prod *= a[right];
-      while (left < right && prod > k) {
-        prod /= a[left++];
-      }
+	public static void main(String[] args) {
 
-      if (prod < k) {
-        count += (right - left + 1);
-      }
-    }
-    return count;
-  }
+		int[] a1 = { 1, 2, 3, 4 };
+		System.out.println(count(a1, 10));
 
-  public static void main(String[] args) {
-    
-    int[] a1 = { 1, 2, 3, 4 };
-    System.out.println(count(a1, 10));
+		int[] a2 = { 1, 9, 2, 8, 6, 4, 3 };
+		System.out.println(count(a2, 100));
 
-    int[] a2 = { 1, 9, 2, 8, 6, 4, 3 };
-    System.out.println(count(a2, 100));
+		int[] a3 = { 5, 3, 2 };
+		System.out.println(count(a3, 16));
 
-    int[] a3 = { 5, 3, 2};
-    System.out.println(count(a3, 16));
+		int[] a4 = { 100, 200 };
+		System.out.println(count(a4, 100));
 
-    int[] a4 = { 100, 200};
-    System.out.println(count(a4, 100));
+		int[] a5 = { 100, 200 };
+		System.out.println(count(a5, 101));
 
-    int[] a5 = { 100, 200};
-    System.out.println(count(a5, 101));
+		int[] a6 = { 10, 5, 2, 6 };
+		System.out.println(count(a6, 100)); // 8
 
-  }
+		int[] a7 = { 1, 2, 3 };
+		System.out.println(count(a7, 0)); // 0
+	}
 }
