0577b705918635fccb6cfa5162d1be635260b91a
svetanis <svet125@gmail.com>
2025-02-20 18:41:13 -0700

    search in rotated array

--- src/main/java/com/svetanis/algorithms/search/binary/rotated/RotatedGivenElementDuplicatesIterative.java
+++ src/main/java/com/svetanis/algorithms/search/binary/rotated/RotatedGivenElementDuplicatesIterative.java
@@ -2,90 +2,90 @@ package com.svetanis.algorithms.search.binary.rotated;
 
 import java.util.List;
 
+// 81. Search in Rotated Sorted Array II
+
 public final class RotatedGivenElementDuplicatesIterative {
+	// Time Complexity: O(log n)
 
-  public static int search(int[] a, int k) {
-    // Time Complexity: O(log n)
+	public static int search(int[] a, int k) {
+		int n = a.length;
+		int left = 0;
+		int right = n - 1;
 
-    int n = a.length;
-    int left = 0;
-    int right = n - 1;
+		while (left <= right) {
+			int mid = left + (right - left) / 2;
+			if (a[mid] == k) {
+				return mid;
+			}
 
-    while (left <= right) {
-      int mid = left + (right - left) / 2;
-      if (a[mid] == k) {
-        return mid;
-      }
+			// if indexes left, mid and right are the same
+			// skip one number from both ends as k != a[mid]
+			if (a[left] == a[mid] && a[right] == a[mid]) {
+				left++;
+				right--;
+			} else if (a[left] <= a[mid]) {
+				// a[left ... mid] sorted
+				if (k >= a[left] && k < a[mid]) {
+					right = mid - 1;
+				} else { // k > a[mid]
+					left = mid + 1;
+				}
+			} else {
+				// a[mid ... right] sorted
+				if (k >= a[mid] && k <= a[right]) {
+					left = mid + 1;
+				} else {
+					right = mid - 1;
+				}
+			}
+		}
+		return -1;
+	}
 
-      // if indexes left, mid and right are the same
-      // skip one number from both ends as k != a[mid]
-      if (a[left] == a[mid] && a[right] == a[mid]) {
-        left++;
-        right--;
-      } else if (a[left] <= a[mid]) {
-    	// a[left ... mid] sorted
-        if (k >= a[left] && k < a[mid]) {
-          right = mid - 1;
-        } else { // k > a[mid]
-          left = mid + 1;
-        }
-      } else {
-        // a[mid ... right] sorted
-        if (k >= a[mid] && k <= a[right]) {
-          left = mid + 1;
-        } else {
-          right = mid - 1;
-        }
-      }
-    }
-    return -1;
-  }
+	public static int search(List<Integer> a, int k) {
+		// Time Complexity: O(log n)
 
-  public static int search(List<Integer> a, int k) {
-    // Time Complexity: O(log n)
+		int n = a.size();
+		int left = 0;
+		int right = n - 1;
 
-    int n = a.size();
-    int left = 0;
-    int right = n - 1;
+		while (left <= right) {
+			int mid = left + (right - left) / 2;
+			if (a.get(mid) == k) {
+				return mid;
+			}
 
-    while (left <= right) {
-      int mid = left + (right - left) / 2;
-      if (a.get(mid) == k) {
-        return mid;
-      }
+			if (a.get(left) == a.get(mid) && a.get(right) == a.get(mid)) {
+				left++;
+				right--;
+			} else if (a.get(left) <= a.get(mid)) {
+				// a[left ... mid] sorted
+				if (k >= a.get(left) && k < a.get(mid)) {
+					right = mid - 1;
+				} else { // k > a[mid]
+					left = mid + 1;
+				}
+			} else {
+				// a[mid ... right] sorted
+				if (k >= a.get(mid) && k <= a.get(right)) {
+					left = mid + 1;
+				} else {
+					right = mid - 1;
+				}
+			}
+		}
+		return -1;
+	}
 
-      
-      if (a.get(left) == a.get(mid) && a.get(right) == a.get(mid)) {
-        left++;
-        right--;
-      } else if (a.get(left) <= a.get(mid)) {
-    	// a[left ... mid] sorted
-        if (k >= a.get(left) && k < a.get(mid)) {
-          right = mid - 1;
-        } else { // k > a[mid]
-          left = mid + 1;
-        }
-      } else {
-        // a[mid ... right] sorted
-        if (k >= a.get(mid) && k <= a.get(right)) {
-          left = mid + 1;
-        } else {
-          right = mid - 1;
-        }
-      }
-    }
-    return -1;
-  }
+	public static void main(String[] args) {
+		int[] a1 = { 5, 6, 7, 8, 9, 10, 1, 2, 3 };
+		System.out.println(search(a1, 3));
 
-  public static void main(String[] args) {
-    int[] a1 = { 5, 6, 7, 8, 9, 10, 1, 2, 3 };
-    System.out.println(search(a1, 3));
+		int[] a2 = { 3, 4, 5, 1, 2 };
+		System.out.println(search(a2, 3));
+		System.out.println(search(a2, 4));
 
-    int[] a2 = { 3, 4, 5, 1, 2 };
-    System.out.println(search(a2, 3));
-    System.out.println(search(a2, 4));
-
-    int[] a3 = { 1, 2, 3, 4 };
-    System.out.println(search(a3, 3));
-  }
+		int[] a3 = { 1, 2, 3, 4 };
+		System.out.println(search(a3, 3));
+	}
 }
--- src/main/java/com/svetanis/algorithms/search/binary/rotated/RotatedGivenElementDuplicatesRecursive.java
+++ src/main/java/com/svetanis/algorithms/search/binary/rotated/RotatedGivenElementDuplicatesRecursive.java
@@ -1,81 +1,127 @@
 package com.svetanis.algorithms.search.binary.rotated;
 
+// 81. Search in Rotated Sorted Array II
+
 public final class RotatedGivenElementDuplicatesRecursive {
 
-  public static int search(int[] a, int k) {
-    int n = a.length;
-    return search(a, 0, n - 1, k);
-  }
+	public static int search(int[] a, int k) {
+		int n = a.length;
+		return search(a, 0, n - 1, k);
+	}
 
-  private static int search(int[] a, int left, int right, int k) {
-    if (right < left) {
-      return -1;
-    }
+	private static int search(int[] a, int left, int right, int k) {
+		if (right < left) {
+			return -1;
+		}
+		int mid = left + (right - left) / 2;
+		if (k == a[mid]) {
+			return mid;
+		}
+		if (a[left] < a[mid]) {
+			// left is sorted
+			if (k >= a[left] && k < a[mid]) {
+				return search(a, left, mid - 1, k);
+			} else {
+				return search(a, mid + 1, right, k);
+			}
+		} else if (a[mid] < a[left]) {
+			// right is sorted
+			if (k >= a[mid] && k <= a[right]) {
+				return search(a, mid + 1, right, k);
+			} else {
+				return search(a, left, mid - 1, k);
+			}
+		}
+		// Left is either all repeats OR loops around
+		// (with the right half being all dups)
+		else if (a[left] == a[mid]) {
+			// if right half is different, search there
+			if (a[mid] != a[right]) {
+				return search(a, mid + 1, right, k);
+			} else {
+				// we have to search both halves
+				int result = search(a, left, mid - 1, k);
+				if (result == -1) {
+					return search(a, mid + 1, right, k);
+				} else {
+					return result;
+				}
+			}
+		}
+		return -1;
+	}
 
-    int mid = left + (right - left) / 2;
+	public boolean searchRotated(int[] a, int k) {
+		int n = a.length;
+		return searchRotated(a, 0, n - 1, k);
+	}
 
-    if (k == a[mid]) {
-      return mid;
-    }
-    
-    if (a[left] < a[mid]) {
-      // left is sorted
-      if (k >= a[left] && k < a[mid]) {
-        return search(a, left, mid - 1, k);
-      } else {
-        return search(a, mid + 1, right, k);
-      }
-    } else if (a[mid] < a[left]) {
-      // right is sorted
-      if (k >= a[mid] && k <= a[right]) {
-        return search(a, mid + 1, right, k);
-      } else {
-        return search(a, left, mid - 1, k);
-      }
-    }
-    // Left is either all repeats OR loops around
-    // (with the right half being all dups)
-    else if (a[left] == a[mid]) {
-      // if right half is different, search there
-      if (a[mid] != a[right]) {
-        return search(a, mid + 1, right, k);
-      } else {
-        // we have to search both halves
-        int result = search(a, left, mid - 1, k);
-        if (result == -1) {
-          return search(a, mid + 1, right, k);
-        } else {
-          return result;
-        }
-      }
-    }
-    return -1;
-  }
+	private boolean searchRotated(int[] a, int left, int right, int k) {
+		if (right < left) {
+			return false;
+		}
+		int mid = left + (right - left) / 2;
+		if (k == a[mid]) {
+			return true;
+		}
+		if (a[left] < a[mid]) {
+			// left is sorted
+			if (k >= a[left] && k < a[mid]) {
+				return searchRotated(a, left, mid - 1, k);
+			} else {
+				return searchRotated(a, mid + 1, right, k);
+			}
+		} else if (a[mid] < a[left]) {
+			// right is sorted
+			if (k >= a[mid] && k <= a[right]) {
+				return searchRotated(a, mid + 1, right, k);
+			} else {
+				return searchRotated(a, left, mid - 1, k);
+			}
+		}
+		// Left is either all repeats OR loops around
+		// (with the right half being all dups)
+		else if (a[left] == a[mid]) {
+			// if right half is different, search there
+			if (a[mid] != a[right]) {
+				return searchRotated(a, mid + 1, right, k);
+			} else {
+				// we have to search both halves
+				boolean result = searchRotated(a, left, mid - 1, k);
+				if (!result) {
+					return searchRotated(a, mid + 1, right, k);
+				} else {
+					return result;
+				}
+			}
+		}
+		return false;
+	}
 
-  public static void main(String[] args) {
-    int[] a1 = { 5, 6, 7, 8, 9, 10, 1, 2, 3 };
-    System.out.println(search(a1, 3));
+	public static void main(String[] args) {
+		int[] a1 = { 5, 6, 7, 8, 9, 10, 1, 2, 3 };
+		System.out.println(search(a1, 3));
 
-    int[] a2 = { 3, 4, 5, 1, 2 };
-    System.out.println(search(a2, 3));
+		int[] a2 = { 3, 4, 5, 1, 2 };
+		System.out.println(search(a2, 3));
 
-    System.out.println(search(a2, 4));
+		System.out.println(search(a2, 4));
 
-    int[] a3 = { 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1 };
-    System.out.println(search(a3, 0));
+		int[] a3 = { 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1 };
+		System.out.println(search(a3, 0));
 
-    int[] a4 = { 2, 3, 0, 2, 2, 2, 2, 2, 2, 2 };
-    System.out.println(search(a4, 3));
-    System.out.println(search(a4, 2));
+		int[] a4 = { 2, 3, 0, 2, 2, 2, 2, 2, 2, 2 };
+		System.out.println(search(a4, 3));
+		System.out.println(search(a4, 2));
 
-    int[] a5 = { 1, 2, 3, 4 };
-    System.out.println(search(a5, 3));
+		int[] a5 = { 1, 2, 3, 4 };
+		System.out.println(search(a5, 3));
 
-    int[] a6 = { 2, 3, 2, 2, 2, 2, 2, 2, 2, 2 };
-    System.out.println(search(a6, 2));
-    System.out.println(search(a6, 3));
-    System.out.println(search(a6, 4));
-    System.out.println(search(a6, 1));
-    System.out.println(search(a6, 8));
-  }
+		int[] a6 = { 2, 3, 2, 2, 2, 2, 2, 2, 2, 2 };
+		System.out.println(search(a6, 2));
+		System.out.println(search(a6, 3));
+		System.out.println(search(a6, 4));
+		System.out.println(search(a6, 1));
+		System.out.println(search(a6, 8));
+	}
 }
