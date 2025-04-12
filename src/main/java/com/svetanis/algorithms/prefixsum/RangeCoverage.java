ce17c3e6a2e757bbf4f6f542edfed23b3e903eb8
svetanis <svet125@gmail.com>
2025-04-12 13:52:26 -0700

    range coverage

---
+++ src/main/java/com/svetanis/algorithms/prefixsum/RangeCoverage.java
@@ -0,0 +1,38 @@
+package com.svetanis.algorithms.prefixsum;
+
+// 1893. Check if All the Integers in a Range Are Covered
+
+public final class RangeCoverage {
+	// Time complexity: O(n)
+
+	public static boolean covered(int[][] ranges, int left, int right) {
+		int[] deltas = deltas(ranges);
+		int coverage = 0;
+		for (int i = 0; i < deltas.length; i++) {
+			coverage += deltas[i];
+			if (i >= left && i <= right && coverage == 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	private static int[] deltas(int[][] ranges) {
+		int[] a = new int[52];
+		for (int[] range : ranges) {
+			int start = range[0];
+			int end = range[1];
+			a[start] += 1;
+			a[end + 1] -= 1;
+		}
+		return a;
+	}
+
+	public static void main(String[] args) {
+		int[][] ranges1 = { { 1, 2 }, { 3, 4 }, { 5, 6 } };
+		System.out.println(covered(ranges1, 2, 5)); // true
+
+		int[][] ranges2 = { { 1, 10 }, { 10, 20 } };
+		System.out.println(covered(ranges2, 21, 21)); // false
+	}
+}
