ce8ed1dd54cdeafe22fce674b43b421b2a2de7b9
svetanis <svet125@gmail.com>
2025-04-15 20:07:58 -0700

    remove vowels

---
+++ src/main/java/com/svetanis/algorithms/string/remove/RemoveVowels.java
@@ -0,0 +1,36 @@
+package com.svetanis.algorithms.string.remove;
+
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+// 1119. Remove Vowels from a String
+
+public final class RemoveVowels {
+	// Time Complexity: O(n)
+	// Space Complexity: O(n)
+
+	public static String removeVowels(String s) {
+		return s.codePoints().mapToObj(c -> (char) c)
+				.filter(Predicate.not(c -> vowel(c)))
+				.map(String::valueOf)
+				.collect(Collectors.joining());
+	}
+
+	public static String remove(String s) {
+		StringBuilder sb = new StringBuilder();
+		for (char c : s.toCharArray()) {
+			if (!vowel(c)) {
+				sb.append(c);
+			}
+		}
+		return sb.toString();
+	}
+
+	private static boolean vowel(char c) {
+		return c == 'a' || c == 'i' || c == 'e' || c == 'o' || c == 'u';
+	}
+
+	public static void main(String[] args) {
+		System.out.println(removeVowels("leetcode")); // ltcd
+	}
+}
