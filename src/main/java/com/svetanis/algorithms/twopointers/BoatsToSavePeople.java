2f106c82a8727a52bfa844879b61a3a5d05d12b3
svetanis <svet125@gmail.com>
2025-04-30 21:18:06 -0700

    boats to save people

---
+++ src/main/java/com/svetanis/algorithms/twopointers/BoatsToSavePeople.java
@@ -0,0 +1,35 @@
+package com.svetanis.algorithms.twopointers;
+
+import java.util.Arrays;
+
+// 283. Move Zeros
+
+public final class BoatsToSavePeople {
+	// Time Complexity: O(n log n)
+	// Space Complexity: O(1)
+
+	public static int countBoats(int[] people, int limit) {
+		Arrays.sort(people);
+		int boats = 0;
+		int n = people.length;
+		for (int left = 0, right = n - 1; left <= right; right--) {
+			if (people[left] + people[right] <= limit) {
+				left++;
+			}
+			boats++;
+		}
+		return boats;
+	}
+
+	public static void main(String[] args) {
+		int[] p1 = { 1, 2 };
+		System.out.println(countBoats(p1, 3)); // 1
+		int[] p2 = { 3, 2, 2, 1 };
+		System.out.println(countBoats(p2, 3)); // 3
+		int[] p3 = { 3, 5, 3, 4 };
+		System.out.println(countBoats(p3, 5)); // 4
+		int[] p4 = { 5, 1, 4, 2 };
+		System.out.println(countBoats(p4, 6)); // 2
+
+	}
+}
