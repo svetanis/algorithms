3ce7631978f2828a558eb0c0e99fba0bbdf55450
svetanis <svet125@gmail.com>
2025-09-05 14:56:48 -0700

    intervals

--- src/main/java/com/svetanis/algorithms/intervals/NonOverlappingIntervals.java
+++ src/main/java/com/svetanis/algorithms/intervals/NonOverlappingIntervals.java
@@ -5,34 +5,48 @@ import java.util.Arrays;
 // 435. Non-overlapping Intervals
 
 public final class NonOverlappingIntervals {
-  // Time Complexity: O(n * log n)
-  // Space Complexity: O(1)
+	// Time Complexity: O(n * log n)
+	// Space Complexity: O(1)
 
-  public static int count(int[][] intervals) {
-    // sort by end time
-    Arrays.sort(intervals, (i1, i2) -> i1[1] - i2[1]);
-    int overlaps = 0;
-    int end = intervals[0][1];
-    for (int i = 1; i < intervals.length; i++) {
-      int start = intervals[i][0];
-      // curr doesn't overlap with prev, update end
-      if (start >= end) {
-        end = intervals[i][1];
-      } else {
-        overlaps++;
-      }
-    }
-    return overlaps;
-  }
+	public static int nonOverlappingIntervals(int[][] intervals) {
+		Arrays.sort(intervals, (a, b) -> a[1] - b[1]);
+		int n = intervals.length;
+		int nonoverlaps = 1;
+		int prevEnd = intervals[0][1];
+		for (int i = 1; i < n; i++) {
+			if (intervals[i][0] >= prevEnd) {
+				prevEnd = intervals[i][1];
+				nonoverlaps++;
+			}
+		}
+		return n - nonoverlaps;
+	}
 
-  public static void main(String[] args) {
-    int[][] intervals1 = { { 1, 2 }, { 2, 3 }, { 3, 4 }, { 1, 3 } };
-    System.out.println(count(intervals1)); // 1
+	public static int count(int[][] intervals) {
+		// sort by end time
+		Arrays.sort(intervals, (i1, i2) -> i1[1] - i2[1]);
+		int overlaps = 0;
+		int end = intervals[0][1];
+		for (int i = 1; i < intervals.length; i++) {
+			int start = intervals[i][0];
+			// curr doesn't overlap with prev, update end
+			if (start >= end) {
+				end = intervals[i][1];
+			} else {
+				overlaps++;
+			}
+		}
+		return overlaps;
+	}
 
-    int[][] intervals2 = { { 1, 2 }, { 1, 2 }, { 1, 2 } };
-    System.out.println(count(intervals2)); // 2
+	public static void main(String[] args) {
+		int[][] intervals1 = { { 1, 2 }, { 2, 3 }, { 3, 4 }, { 1, 3 } };
+		System.out.println(count(intervals1)); // 1
 
-    int[][] intervals3 = { { 1, 2 }, { 2, 3 } };
-    System.out.println(count(intervals3)); // 0
-  }
+		int[][] intervals2 = { { 1, 2 }, { 1, 2 }, { 1, 2 } };
+		System.out.println(count(intervals2)); // 2
+
+		int[][] intervals3 = { { 1, 2 }, { 2, 3 } };
+		System.out.println(count(intervals3)); // 0
+	}
 }
