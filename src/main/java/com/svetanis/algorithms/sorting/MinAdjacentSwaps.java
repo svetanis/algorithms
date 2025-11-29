6ce926cfa5ceacd9738d0e5e99afdcd6d81e527b
svetanis <svet125@gmail.com>
2025-11-28 20:31:15 -0700

    Min adjacent swaps

--- src/main/java/com/svetanis/algorithms/sorting/MinAdjacentSwaps.java
+++ src/main/java/com/svetanis/algorithms/sorting/MinAdjacentSwaps.java
@@ -1,55 +1,35 @@
 package com.svetanis.algorithms.sorting;
 
-import static java.lang.Integer.MAX_VALUE;
-import static java.lang.Integer.MIN_VALUE;
-import static java.util.Arrays.asList;
-
-import java.util.List;
-
 // 2340. Minimum Adjacent Swaps to Make a Valid Array
 
 public final class MinAdjacentSwaps {
 	// Time complexity: O(n)
 
-	public static int minAdjacentSwaps(List<Integer> list) {
-		int n = list.size();
-		int min = min(list);
-		int max = max(list);
-		if(min == max) {
+	public static int minAdjacentSwaps(int[] a) {
+		int n = a.length;
+		int min = Integer.MAX_VALUE;
+		int max = Integer.MIN_VALUE;
+		int minIndex = 0;
+		int maxIndex = 0;
+		for (int i = 0; i < n; i++) {
+			if (a[i] < min) {
+				min = a[i];
+				minIndex = i;
+			}
+			if (a[i] >= max) {
+				max = a[i];
+				maxIndex = i;
+			}
+		}
+		if (minIndex == maxIndex) {
 			return 0;
 		}
-		int swap = min > max ? 1 : 0;
-		return min + n - 1 - max - swap;
-	}
-
-	private static int min(List<Integer> list) {
-		int min = MAX_VALUE;
-		int index = 0;
-		for (int i = 0; i < list.size(); i++) {
-			int curr = list.get(i);
-			if (curr < min) {
-				min = curr;
-				index = i;
-			}
-		}
-		return index;
-	}
-
-	private static int max(List<Integer> list) {
-		int max = MIN_VALUE;
-		int index = 0;
-		for (int i = 0; i < list.size(); i++) {
-			int curr = list.get(i);
-			if (curr > max) {
-				max = curr;
-				index = i;
-			}
-		}
-		return index;
+		int swap = minIndex > maxIndex ? 1 : 0;
+		return minIndex + n - 1 - maxIndex - swap;
 	}
 
 	public static void main(String[] args) {
-		List<Integer> list = asList(3, 1, 2, 4);
-		System.out.println(minAdjacentSwaps(list)); // 1
+		int[] a1 = { 3, 4, 5, 5, 3, 1 };
+		System.out.println(minAdjacentSwaps(a1)); // 6
 	}
 }
