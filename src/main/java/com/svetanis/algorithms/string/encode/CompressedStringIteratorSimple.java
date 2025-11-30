44e23e2e9c8dd9d7ee6a258e661f103e8fb86eb3
svetanis <svet125@gmail.com>
2025-11-29 18:16:15 -0700

    compressed string iterator

--- src/main/java/com/svetanis/algorithms/string/encode/CompressedStringIteratorSimple.java
+++ src/main/java/com/svetanis/algorithms/string/encode/CompressedStringIteratorSimple.java
@@ -1,40 +1,38 @@
 package com.svetanis.algorithms.string.encode;
 
-import static java.lang.Character.isDigit;
-
 // 604. Design Compressed String Iterator
 
 public final class CompressedStringIteratorSimple {
 
 	public CompressedStringIteratorSimple(String s) {
-		this.sb = compressed(s);
+		this.c = ' ';
+		this.str = s;
+		this.index = 0;
+		this.count = 0;
 	}
 
+	private char c;
 	private int index;
-	private StringBuilder sb = new StringBuilder();
-
-	private StringBuilder compressed(String s) {
-		int index = 0;
-		int len = s.length();
-		while (index < len) {
-			char c = s.charAt(index);
-			int count = 0;
-			while (++index < len && isDigit(s.charAt(index))) {
-				count = count * 10 + (s.charAt(index) - '0');
-			}
-			for (int j = 0; j < count; j++) {
-				sb.append(c);
-			}
-		}
-		return sb;
-	}
+	private int count;
+	private String str;
 
 	public char next() {
-		return !hasNext() ? ' ' : sb.charAt(index++);
+		if (!hasNext()) {
+			return ' ';
+		}
+		if (count == 0) {
+			c = str.charAt(index++);
+			while (index < str.length() && Character.isDigit(str.charAt(index))) {
+				count = count * 10 + str.charAt(index) - '0';
+				index += 1;
+			}
+		}
+		count -= 1;
+		return c;
 	}
 
 	public boolean hasNext() {
-		return index < sb.length();
+		return index != str.length() || count != 0;
 	}
 
 	public static void main(String[] args) {
