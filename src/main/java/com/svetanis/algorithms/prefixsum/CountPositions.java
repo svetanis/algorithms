4077b637f71ba2ab93c5125fcf6d69c48fb4bd2d
svetanis <svet125@gmail.com>
2025-12-04 20:15:24 -0700

    count positions'

--- src/main/java/com/svetanis/algorithms/prefixsum/CountPositions.java
+++ src/main/java/com/svetanis/algorithms/prefixsum/CountPositions.java
@@ -5,19 +5,40 @@ package com.svetanis.algorithms.prefixsum;
 public final class CountPositions {
 	// Time complexity: O(n + m)
 
+	public static int mr(int n, int[][] lights, int[] requirement) {
+		int[] line = new int[n];
+		for (int[] light : lights) {
+			int pos = light[0], range = light[1];
+			int left = Math.max(0, pos - range);
+			line[left]++;
+			if (pos + range + 1 < n) {
+				line[pos + range + 1]--;
+			}
+		}
+		int sum = 0;
+		int count = 0;
+		for (int i = 0; i < n; i++) {
+			sum += line[i];
+			if (sum >= requirement[i]) {
+				count++;
+			}
+		}
+		return count;
+	}
+
 	public static int meetRequirement(int n, int[][] lights, int[] requirement) {
-		int[] brightness = new int[100010];
+		int[] line = new int[100010];
 		for (int[] light : lights) {
 			int left = Math.max(0, light[0] - light[1]);
 			int right = Math.min(n - 1, light[0] + light[1]);
-			brightness[left]++;
-			brightness[right + 1]--;
+			line[left]++;
+			line[right + 1]--;
 		}
 		int count = 0;
-		int currb = 0;
+		int sum = 0;
 		for (int i = 0; i < n; i++) {
-			currb += brightness[i];
-			if (currb >= requirement[i]) {
+			sum += line[i];
+			if (sum >= requirement[i]) {
 				count++;
 			}
 		}
