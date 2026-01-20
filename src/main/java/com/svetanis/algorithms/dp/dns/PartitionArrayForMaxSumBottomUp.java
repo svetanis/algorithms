459d9123cb19db02560a48db3e2ca512b56448c2
svetanis <svet125@gmail.com>
2026-01-20 16:04:24 -0700

    partition array for max sum

--- src/main/java/com/svetanis/algorithms/dp/dns/PartitionArrayForMaxSumBottomUp.java
+++ src/main/java/com/svetanis/algorithms/dp/dns/PartitionArrayForMaxSumBottomUp.java
@@ -2,11 +2,26 @@ package com.svetanis.algorithms.dp.dns;
 
 // 1043. Partition Array for Maximum Sum
 
-public final class PartitionArrayForMaxSum {
+public final class PartitionArrayForMaxSumBottomUp {
 	// Time Complexity: O(n * k)
 	// Space Complexity: O(n)
 
 	public static int maxSum(int[] a, int k) {
+		int n = a.length;
+		int[] dp = new int[n + 1];
+		for (int start = n - 1; start >= 0; start--) {
+			int max = 0;
+			int end = Math.min(n, start + k);
+			for (int j = start; j < end; j++) {
+				max = Math.max(max, a[j]);
+				int sum = dp[j + 1] + max * (j - start + 1);
+				dp[start] = Math.max(dp[start], sum);
+			}
+		}
+		return dp[0];
+	}
+
+	public static int maxSum2(int[] a, int k) {
 		int n = a.length;
 		int[] dp = new int[n + 1];
 		for (int i = 1; i <= n; i++) {
@@ -14,6 +29,20 @@ public final class PartitionArrayForMaxSum {
 			for (int j = i; j > Math.max(0, i - k); j--) {
 				max = Math.max(max, a[j - 1]);
 				int sum = dp[j - 1] + max * (i - j + 1);
+				dp[i] = Math.max(dp[i], sum);
+			}
+		}
+		return dp[n];
+	}
+
+	public static int maxSum3(int[] a, int k) {
+		int n = a.length;
+		int[] dp = new int[n + 1];
+		for (int i = 1; i <= n; i++) {
+			int max = 0;
+			for (int j = 1; j <= Math.min(k, i); j++) {
+				max = Math.max(max, a[i - j]);
+				int sum = dp[i - j] + max * j;
 				dp[i] = Math.max(dp[i], sum);
 			}
 		}
