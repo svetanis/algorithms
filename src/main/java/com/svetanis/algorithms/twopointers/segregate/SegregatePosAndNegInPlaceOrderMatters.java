8530cdf8fc1aebe8eb4b89d61b4887826e1e688c
svetanis <svet125@gmail.com>
2026-08-15 13:53:45 -0700

    moving two-pointer problems from data-structures to algos repo

---
+++ src/main/java/com/svetanis/algorithms/twopointers/segregate/SegregatePosAndNegInPlaceOrderMatters.java
@@ -0,0 +1,48 @@
+package com.svetanis.algorithms.twopointers.segregate;
+
+import static com.svetanis.java.base.utils.Print.print;
+import static com.svetanis.java.base.utils.Swap.swap;
+
+// given unsorted array of both positive and negative numbers
+// rearrange it such that all negative integers appear before 
+// all the positive integers in place; order matters
+
+public final class SegregatePosAndNegInPlaceOrderMatters {
+
+	public static void segregate(int[] a) {
+		// Time complexity: O(n)
+		// Auxiliary Space: O(1)
+
+		int count = countNegatives(a);
+
+		// move negatives to front
+		int i = 0;
+		int j = i + 1;
+		while (i < count) {
+			if (a[i] < 0) {
+				i++;
+				j = i + 1;
+			} else if (a[i] > 0 && j < a.length) {
+				swap(a, i, j);
+				j++;
+			}
+		}
+	}
+
+	private static int countNegatives(int[] a) {
+		int count = 0;
+		// count negatives
+		for (int val : a) {
+			if (val < 0) {
+				count++;
+			}
+		}
+		return count;
+	}
+
+	public static void main(String[] args) {
+		int[] a = { -12, 11, -13, -5, 6, -7, 5, -3, -6 };
+		segregate(a);
+		print(a);
+	}
+}
